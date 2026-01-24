129026f3cdf65a152d3557a962911d9e01588b76
kainoa-h <kainoahowj@gmail.com>
2026-01-24 20:41:58 +0800

    80

---
+++ rust/src/bin/80_remove_duplicates_from_sorted_array_II.rs
@@ -0,0 +1,42 @@
+struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut real_idx = 1;
+        let mut prev_num = nums[0];
+        let mut seen_count: i8 = 1;
+        for i in 1..nums.len() {
+            let curr_num = nums[i];
+            if curr_num != prev_num {
+                nums[real_idx] = curr_num;
+                real_idx += 1;
+                prev_num = curr_num;
+                seen_count = 0;
+            } else if seen_count == 1 {
+                nums[real_idx] = curr_num;
+                real_idx += 1;
+            }
+            seen_count += 1;
+        }
+        real_idx as i32
+    }
+}
+
+fn what(mut nums: Vec<i32>) {
+    println!("{:?}", Solution::remove_duplicates(&mut nums));
+    println!("{:?}", nums);
+}
+
+fn main() {
+    // Input: nums = [1,1,1,2,2,3]
+    // Output: 5, nums = [1,1,2,2,3,_]
+    what(vec![1, 1, 1, 2, 2, 3]);
+
+    // Input: nums = [0,0,1,1,1,1,2,3,3]
+    // Output: 7, nums = [0,0,1,1,2,3,3,_,_]
+    what(vec![0, 0, 1, 1, 1, 1, 2, 3, 3]);
+
+    what(vec![1, 1, 1]);
+    what(vec![1, 1]);
+    what(vec![1]);
+}
