569dd222c3d0f5d4885012089291ce43f5873782
kainoa-h <kainoahowj@gmail.com>
2026-01-25 16:10:01 +0800

    45

---
+++ rust/src/bin/45_jump_game.rs
@@ -0,0 +1,31 @@
+struct Solution {}
+
+impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        if nums.len() == 1 {
+            return 0;
+        }
+
+        let mut max_reachable = 0;
+        let mut current_range = 0;
+        let mut jumps = 0;
+        for (i, &n) in nums.iter().enumerate() {
+            max_reachable = max_reachable.max(i + n as usize);
+            if i == current_range {
+                jumps += 1;
+                current_range = max_reachable;
+            }
+            if current_range + 1 >= nums.len() {
+                return jumps;
+            }
+        }
+        jumps
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::jump(vec![2, 3, 1, 1, 4]), 2);
+    assert_eq!(Solution::jump(vec![3, 2, 1, 0, 4]), 2);
+    assert_eq!(Solution::jump(vec![0]), 0);
+    assert_eq!(Solution::jump(vec![1, 0]), 1);
+}
