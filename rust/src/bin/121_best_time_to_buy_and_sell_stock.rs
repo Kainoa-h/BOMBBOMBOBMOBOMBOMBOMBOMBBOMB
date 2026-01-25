6c7ed6a42435d29b416066f4cd8bd146dc407c27
kainoa-h <kainoahowj@gmail.com>
2026-01-25 13:32:19 +0800

    121: idomatic refactor

--- rust/src/bin/121_best_time_to_buy_and_sell_stock.rs
+++ rust/src/bin/121_best_time_to_buy_and_sell_stock.rs
@@ -2,18 +2,12 @@ struct Solution {}
 
 impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
-        let mut min = prices[0];
+        let mut min = i32::MAX;
         let mut max_profit = 0;
 
-        for &p in prices.iter().skip(1) {
-            if min > p {
-                min = p;
-                continue;
-            }
-            let diff = p - min;
-            if diff > max_profit {
-                max_profit = diff;
-            }
+        for p in prices {
+            min = min.min(p);
+            max_profit = max_profit.max(p - min);
         }
 
         max_profit
