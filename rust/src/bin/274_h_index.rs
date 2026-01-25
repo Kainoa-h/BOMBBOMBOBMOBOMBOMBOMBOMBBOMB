93f9625414f5adfed48b545860297d9f3d4331e6
kainoa-h <kainoahowj@gmail.com>
2026-01-26 01:15:33 +0800

    274: bucket sort
    time: O(n)
    space: O(n)

--- rust/src/bin/274_h_index.rs
+++ rust/src/bin/274_h_index.rs
@@ -1,31 +1,49 @@
-use std::cmp::Reverse;
-
 struct Solution {}
 
 impl Solution {
-    // After sorting desc
-    // The idx + 1 tells us at least how many papers have (idx + 1) citations
-    pub fn h_index(mut citations: Vec<i32>) -> i32 {
-        citations.sort_unstable_by_key(|x| Reverse(*x));
-        citations
-            .iter()
-            .enumerate()
-            .take_while(|(idx, count)| **count as usize > *idx)
-            .count() as i32
+    pub fn h_index(citations: Vec<i32>) -> i32 {
+        let mut buckets: Vec<i32> = vec![0; citations.len()];
+        for &c in &citations {
+            if c == 0 {
+                continue;
+            }
+            if c >= citations.len() as i32 {
+                buckets[citations.len() - 1] += 1;
+            } else {
+                println!("{} to index {}", c, c - 1);
+                buckets[c as usize - 1] += 1;
+            }
+        }
+        println!("{:?}", buckets);
+
+        let mut sum = 0;
+        for (idx, &count) in buckets.iter().enumerate().rev() {
+            sum += count;
+            println!("current sum: {}, current idx: {}", sum, idx);
+            if sum > idx as i32 {
+                return idx as i32 + 1; // reset index to 0 based equivilant
+            }
+        }
+        0
     }
 }
 
+fn what(citations: Vec<i32>, ans: i32) {
+    println!("---{:?}---", citations);
+    assert_eq!(Solution::h_index(citations), ans);
+}
+
 fn main() {
-    assert_eq!(Solution::h_index(vec![3, 0, 6, 1, 5]), 3);
-    assert_eq!(Solution::h_index(vec![1, 3, 1]), 1);
-    assert_eq!(Solution::h_index(vec![100]), 1);
-    assert_eq!(Solution::h_index(vec![100, 200]), 2);
-    assert_eq!(Solution::h_index(vec![1, 1, 1]), 1);
-    assert_eq!(Solution::h_index(vec![0, 0, 0]), 0);
-    assert_eq!(Solution::h_index(vec![0, 0, 2]), 1);
-    assert_eq!(Solution::h_index(vec![2, 2, 2]), 2);
-    assert_eq!(Solution::h_index(vec![2, 2, 2, 0]), 2);
-    assert_eq!(Solution::h_index(vec![1, 7, 9, 4]), 3);
-    assert_eq!(Solution::h_index(vec![9, 7, 4, 1]), 3);
-    assert_eq!(Solution::h_index(vec![9, 7, 7, 5, 1]), 4);
+    what(vec![3, 0, 6, 1, 5], 3);
+    what(vec![1, 3, 1], 1);
+    what(vec![100], 1);
+    what(vec![100, 200], 2);
+    what(vec![1, 1, 1], 1);
+    what(vec![0, 0, 0], 0);
+    what(vec![0, 0, 2], 1);
+    what(vec![2, 2, 2], 2);
+    what(vec![2, 2, 2, 0], 2);
+    what(vec![1, 7, 9, 4], 3);
+    what(vec![9, 7, 4, 1], 3);
+    what(vec![9, 7, 7, 5, 1], 4);
 }
