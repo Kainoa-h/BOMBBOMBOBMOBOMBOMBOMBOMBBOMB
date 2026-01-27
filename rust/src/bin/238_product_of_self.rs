0deed942fd7ebc8199ac7ffa783c50359e8fd00d
kainoa-h <kainoahowj@gmail.com>
2026-01-27 22:29:48 +0800

    238: one less vec
    time: O(n)
    space: O(n)

--- rust/src/bin/238_product_of_self.rs
+++ rust/src/bin/238_product_of_self.rs
@@ -2,30 +2,15 @@ struct Solution {}
 
 impl Solution {
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        let mut prefix_list: Vec<i32> = Vec::with_capacity(nums.len());
-        prefix_list.push(*nums.first().unwrap());
-        let mut accumulator = prefix_list[0];
-        for &n in nums.iter().skip(1) {
-            accumulator *= n;
-            prefix_list.push(accumulator);
+        let mut output = vec![1; nums.len()];
+        for i in 0..nums.len() - 1 {
+            output[i + 1] = output[i] * nums[i];
         }
-
-        let mut postfix_list: Vec<i32> = vec![0; nums.len()];
-        *postfix_list.last_mut().unwrap() = *nums.last().unwrap();
-        accumulator = *postfix_list.last().unwrap();
-        for (idx, &n) in nums.iter().enumerate().rev().skip(1) {
-            accumulator *= n;
-            postfix_list[idx] = accumulator;
+        let mut accumulator = 1;
+        for i in (0..nums.len()).rev() {
+            output[i] *= accumulator;
+            accumulator *= nums[i];
         }
-
-        let mut output = Vec::with_capacity(nums.len());
-        output.push(postfix_list[1]);
-        for idx in 1..nums.len() - 1 {
-            let pre = prefix_list[idx - 1];
-            let post = postfix_list[idx + 1];
-            output.push(pre * post);
-        }
-        output.push(prefix_list[prefix_list.len() - 2]);
 
         output
     }
