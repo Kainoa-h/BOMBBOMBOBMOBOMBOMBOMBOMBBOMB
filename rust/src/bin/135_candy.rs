5f6c4c2c3286aaaba79e1d16d38fedcf577c5dcc
kainoa-h <kainoahowj@gmail.com>
2026-01-28 08:04:56 +0800

    135
    time: O(n)
    space: O(n)

---
+++ rust/src/bin/135_candy.rs
@@ -0,0 +1,35 @@
+struct Solution {}
+
+impl Solution {
+    pub fn candy(ratings: Vec<i32>) -> i32 {
+        if ratings.len() == 1 {
+            return 1;
+        }
+
+        let mut candies: Vec<i32> = vec![1; ratings.len()];
+        for i in 1..ratings.len() {
+            if ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1] {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        for i in (0..ratings.len()).rev().skip(1) {
+            if ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1] {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+
+        candies.iter().sum()
+    }
+}
+
+fn what(ratings: Vec<i32>, ans: i32) {
+    println!("{:?}", ratings);
+    assert_eq!(Solution::candy(ratings), ans);
+}
+
+fn main() {
+    what(vec![1, 0, 2], 5);
+    what(vec![1, 2, 2], 4);
+    what(vec![1], 1);
+    what(vec![1, 2, 87, 87, 87, 2, 1], 13);
+}
