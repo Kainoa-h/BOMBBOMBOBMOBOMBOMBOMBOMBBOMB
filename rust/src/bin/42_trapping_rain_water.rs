1f447456982af4bdcc4f705f2c0cd15228b1c257
kainoa-h <kainoahowj@gmail.com>
2026-01-28 20:54:43 +0800

    42: two poiners
    time: O(n)
    space: O(1)

--- rust/src/bin/42_trapping_rain_water.rs
+++ rust/src/bin/42_trapping_rain_water.rs
@@ -1,52 +1,31 @@
 struct Solution {}
 impl Solution {
     pub fn trap(height: Vec<i32>) -> i32 {
-        let mut left_idx = match height.iter().position(|x| *x > 0) {
-            Some(idx) => idx,
-            None => return 0,
-        };
-
-        let prefix_vol: Vec<i32> = height
-            .iter()
-            .scan(0, |acc, &h| {
-                *acc += h;
-                Some(*acc)
-            })
-            .collect();
-
-        let mut total_volume = 0;
-        let mut left_height = height[left_idx];
-        let mut i = left_idx + 1;
-        while i < height.len() - 1 {
-            let mut vol = 0;
-            let mut fallback_height = 0;
-            let mut fallback_idx = 0;
-            for idx in i.clone()..height.len() {
-                let h = height[idx];
-                if left_height <= h {
-                    i = idx; //will be incremented later
-                    vol = ((idx - left_idx - 1) as i32 * left_height)
-                        - (prefix_vol[idx - 1] - prefix_vol[left_idx]);
-                    left_height = h;
-                    left_idx = idx;
-                    break;
+        let mut left_idx = 0;
+        let mut left_max = 0;
+        let mut right_idx = height.len() - 1;
+        let mut right_max = 0;
+        let mut total_vol = 0;
+        while left_idx < right_idx {
+            let left_height = height[left_idx];
+            let right_height = height[right_idx];
+            if left_height < right_height {
+                if left_height >= left_max {
+                    left_max = left_height;
+                } else {
+                    total_vol += left_max - left_height;
                 }
-                if h > fallback_height {
-                    fallback_height = h;
-                    fallback_idx = idx;
+                left_idx += 1;
+            } else {
+                if right_height >= right_max {
+                    right_max = right_height;
+                } else {
+                    total_vol += right_max - right_height;
                 }
+                right_idx -= 1;
             }
-            if vol == 0 && fallback_height != 0 {
-                i = fallback_idx;
-                vol = ((fallback_idx - left_idx - 1) as i32 * fallback_height)
-                    - (prefix_vol[fallback_idx - 1] - prefix_vol[left_idx]);
-                left_height = fallback_height;
-                left_idx = fallback_idx;
-            }
-            total_volume += vol;
-            i += 1;
         }
-        total_volume
+        total_vol
     }
 }
 
@@ -55,8 +34,8 @@ fn what(height: Vec<i32>, ans: i32) {
     assert_eq!(Solution::trap(height), ans);
 }
 fn main() {
-    // what(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6);
-    // what(vec![4, 2, 0, 3, 2, 5], 9);
-    // what(vec![4, 2, 3], 1);
+    what(vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6);
+    what(vec![4, 2, 0, 3, 2, 5], 9);
+    what(vec![4, 2, 3], 1);
     what(vec![5, 4, 1, 2], 1);
 }
