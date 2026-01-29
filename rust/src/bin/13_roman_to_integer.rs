6d1ea7d6566ddfa2ad4354bd36a1223d724a52c6
kainoa-h <kainoahowj@gmail.com>
2026-01-29 21:34:12 +0800

    13: more idomatic rust
    time: O(n)
    space: O(1)

--- rust/src/bin/13_roman_to_integer.rs
+++ rust/src/bin/13_roman_to_integer.rs
@@ -1,25 +1,21 @@
-use std::collections::HashMap;
-
 struct Solution {}
 impl Solution {
     pub fn roman_to_int(s: String) -> i32 {
-        let map = [
-            ('I', 1),
-            ('V', 5),
-            ('X', 10),
-            ('L', 50),
-            ('C', 100),
-            ('D', 500),
-            ('M', 1000),
-        ]
-        .iter()
-        .cloned()
-        .collect::<HashMap<char, i32>>();
+        let map = |c: char| match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0,
+        };
 
         let mut total = 0;
         let mut prev = 0;
         for (_, c) in s.char_indices() {
-            let x = *map.get(&c).unwrap();
+            let x = map(c);
             total += x;
             if prev < x {
                 total -= 2 * prev;
