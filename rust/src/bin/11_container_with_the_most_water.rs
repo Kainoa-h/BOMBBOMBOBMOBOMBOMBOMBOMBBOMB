85575fe3cd63f63baa03ed04a0b8c7f3d52c3673
kainoa-h <kainoahowj@gmail.com>
2026-02-05 17:52:24 +0800

    11
    time: O(n)
    space: O(1)

---
+++ rust/src/bin/11_container_with_the_most_water.rs
@@ -0,0 +1,33 @@
+struct Solution {}
+impl Solution {
+    pub fn max_area(height: Vec<i32>) -> i32 {
+        let mut l = 0;
+        let mut r = height.len() - 1;
+        let mut largest = 0;
+        while l < r {
+            let left = height[l];
+            let right = height[r];
+            let vol = left.min(right) * (r - l) as i32;
+            if vol > largest {
+                largest = vol;
+            }
+            if left < right {
+                l += 1;
+            } else {
+                r -= 1;
+            }
+        }
+
+        largest
+    }
+}
+
+fn what(height: Vec<i32>, ans: i32) {
+    println!("{:?}", height);
+    assert_eq!(Solution::max_area(height), ans)
+}
+
+fn main() {
+    what(vec![1, 8, 6, 2, 5, 4, 8, 3, 7], 49);
+    what(vec![1, 1], 1);
+}
