eb1b18ae71f4bf6e811926454b2991e041a041bf
kainoa-h <kainoahowj@gmail.com>
2026-02-05 17:31:48 +0800

    167: two pointers
    time: O(n)
    space: O(1)

--- rust/src/bin/167_two_sum_II_input_array_sorted.rs
+++ rust/src/bin/167_two_sum_II_input_array_sorted.rs
@@ -2,24 +2,27 @@ struct Solution {}
 
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        for out_idx in 0..numbers.len() {
-            for in_idx in (out_idx + 1)..numbers.len() {
-                let sum = numbers[out_idx] + numbers[in_idx];
-                if sum > target {
-                    break;
-                }
+        let mut l = 0_usize;
+        let mut r = numbers.len() - 1;
 
-                if sum == target {
-                    return vec![(out_idx + 1) as i32, (in_idx + 1) as i32];
-                }
+        while l < r {
+            let sum = numbers[l] + numbers[r];
+            if sum == target {
+                return vec![(l + 1) as i32, (r + 1) as i32];
+            }
+            if sum > target {
+                r -= 1;
+            } else {
+                l += 1;
             }
         }
-        panic!("invalid");
+        panic!()
     }
 }
 
 fn main() {
     assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![1, 2]);
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 26), vec![3, 4]);
     assert_eq!(Solution::two_sum(vec![2, 3, 4], 6), vec![1, 3]);
     assert_eq!(Solution::two_sum(vec![-1, 0, 4], -1), vec![1, 2]);
 }
