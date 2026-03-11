c18ec7e0964a52847a900436c85ebc995412762b
kainoa-h <kainoahowj@gmail.com>
2026-03-11 23:14:17 +0800

    209: more idomatic
    time: O(n)
    space: O(1)

--- rust/src/bin/209_minimum_size_subarray_sum.rs
+++ rust/src/bin/209_minimum_size_subarray_sum.rs
@@ -1,29 +1,22 @@
 struct Solution {}
 impl Solution {
     pub fn min_sub_array_len(target: i32, nums: Vec<i32>) -> i32 {
+        let mut min_len = usize::MAX;
+        let mut current_sum = 0;
         let mut start_index = 0_usize;
-        let mut end_index = 0_usize;
-        let mut min_len = i32::MAX;
-        let mut current_sum = *nums.first().unwrap();
-        loop {
-            if end_index == nums.len() || start_index > end_index {
-                break;
-            }
-
-            if current_sum >= target {
-                min_len = min_len.min((end_index - start_index) as i32 + 1);
+        for (end_index, &num) in nums.iter().enumerate() {
+            current_sum += num;
+            while current_sum >= target {
+                min_len = min_len.min(end_index - start_index + 1);
                 current_sum -= nums[start_index];
                 start_index += 1;
-            } else {
-                end_index += 1;
-                if let Some(num) = nums.get(end_index) {
-                    current_sum += num;
-                } else {
-                    break;
-                }
             }
         }
-        if min_len == i32::MAX { 0 } else { min_len }
+        if min_len == usize::MAX {
+            0
+        } else {
+            min_len as i32
+        }
     }
 }
 
