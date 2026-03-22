d6a0493f4a081f4f4f9582dc95ed2bebefa4dc19
kainoa-h <kainoahowj@gmail.com>
2026-03-22 15:23:27 +0800

    3212: 1D prefix optimisation
    time: O(n)
    space: O(n)

--- rust/src/bin/3212_count_submatrices_with_equal_frequency_of_x_and_y.rs
+++ rust/src/bin/3212_count_submatrices_with_equal_frequency_of_x_and_y.rs
@@ -9,25 +9,24 @@ struct Node {
 impl Solution {
     pub fn number_of_submatrices(grid: Vec<Vec<char>>) -> i32 {
         let mut count = 0;
-        let rows = grid.len();
         let cols = grid[0].len();
-        let mut prefix_grid = vec![vec![Node::default(); cols + 1]; rows + 1];
-        for r_idx in 0..rows {
-            for c_idx in 0..cols {
-                let char = grid[r_idx][c_idx];
-                let val = match char {
+        let mut prefix_row = vec![Node::default(); cols];
+        for row in grid {
+            let mut row_sum = 0;
+            let mut row_has_x = false;
+            for (c_idx, &char) in row.iter().enumerate() {
+                row_sum += match char {
                     'X' => 1,
                     'Y' => -1,
                     _ => 0,
                 };
+                if char == 'X' {
+                    row_has_x = true;
+                }
 
-                let sum = prefix_grid[r_idx][c_idx + 1].sum + prefix_grid[r_idx + 1][c_idx].sum
-                    - prefix_grid[r_idx][c_idx].sum
-                    + val;
-                let has_x = prefix_grid[r_idx][c_idx + 1].has_x
-                    || prefix_grid[r_idx + 1][c_idx].has_x
-                    || val == 1;
-                prefix_grid[r_idx + 1][c_idx + 1] = Node { sum, has_x };
+                let sum = prefix_row[c_idx].sum + row_sum;
+                let has_x = prefix_row[c_idx].has_x || row_has_x;
+                prefix_row[c_idx] = Node { sum, has_x };
 
                 if sum == 0 && has_x {
                     count += 1;
