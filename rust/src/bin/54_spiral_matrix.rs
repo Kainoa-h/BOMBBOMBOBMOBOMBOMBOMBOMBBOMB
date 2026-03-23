caf1f183d71376a6dab117d07e5ad31e50c024bd
kainoa-h <kainoahowj@gmail.com>
2026-03-23 21:47:21 +0800

    54
    time: O(n*m)
    space: O(1)

--- rust/src/bin/54_spiral_matrix.rs
+++ rust/src/bin/54_spiral_matrix.rs
@@ -1,61 +1,73 @@
 struct Solution {}
 impl Solution {
     pub fn spiral_order(matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        let right_bound = matrix[0].len();
-        let down_bound = matrix.len();
+        let rows = matrix.len();
+        let cols = matrix[0].len();
 
-        let mut vertical_bounds_offset = 0;
-        let mut horizontal_bounds_offset = 0;
+        let mut cells = rows * cols;
         let mut result: Vec<i32> = Vec::new();
         let mut row_idx = 0_usize;
         let mut col_idx = 0_usize;
-        result.push(matrix[0][0]);
-        loop {
-            // add to end of right
-            for c_i in (col_idx + 1)..(right_bound - horizontal_bounds_offset) {
-                result.push(matrix[row_idx][c_i]);
+        let (mut buffer_right, mut buffer_bot, mut buffer_left, mut buffer_top) =
+            (0_usize, 0_usize, 0_usize, 1_usize);
+        while cells > 0 {
+            // right
+            for i in (col_idx)..(cols - buffer_right) {
+                result.push(matrix[row_idx][i]);
+                cells -= 1;
             }
-            col_idx = right_bound - horizontal_bounds_offset - 1;
-
-            // add to end of down
-            let mut went_down = false;
-            for r_i in (row_idx + 1)..(down_bound - vertical_bounds_offset) {
-                went_down = true;
-                result.push(matrix[r_i][col_idx]);
-            }
-            row_idx = down_bound - vertical_bounds_offset - 1;
-            vertical_bounds_offset += 1;
-
-            if !went_down {
+            if cells == 0 {
                 break;
             }
+            col_idx = cols - buffer_right - 1;
+            buffer_right += 1;
 
-            // add to end of left
-            let mut went_left = false;
-            for c_i in ((horizontal_bounds_offset)..col_idx).rev() {
-                went_left = true;
-                result.push(matrix[row_idx][c_i]);
+            // down
+            for i in (row_idx + 1)..(rows - buffer_bot) {
+                result.push(matrix[i][col_idx]);
+                cells -= 1;
             }
-            col_idx = horizontal_bounds_offset;
-            horizontal_bounds_offset += 1;
+            if cells == 0 {
+                break;
+            }
+            row_idx = rows - buffer_bot - 1;
+            buffer_bot += 1;
 
-            // add to end of up
-            if went_left {
-                for r_i in ((vertical_bounds_offset)..row_idx).rev() {
-                    result.push(matrix[r_i][col_idx]);
-                }
-                row_idx = vertical_bounds_offset;
+            // left
+            for i in (buffer_left..col_idx).rev() {
+                result.push(matrix[row_idx][i]);
+                cells -= 1;
             }
+            if cells == 0 {
+                break;
+            }
+            col_idx = buffer_left;
+            buffer_left += 1;
+
+            // up
+            for i in (buffer_top..row_idx).rev() {
+                result.push(matrix[i][col_idx]);
+                cells -= 1;
+            }
+            if cells == 0 {
+                break;
+            }
+            row_idx = buffer_top;
+            buffer_top += 1;
+            col_idx += 1;
         }
+
         result
     }
 }
 
 fn main() {
+    println!("3x3");
     assert_eq!(
         vec![1, 2, 3, 6, 9, 8, 7, 4, 5],
         Solution::spiral_order(vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]])
     );
+    println!("3x4");
     assert_eq!(
         vec![1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7],
         Solution::spiral_order(vec![
@@ -64,6 +76,7 @@ fn main() {
             vec![9, 10, 11, 12],
         ])
     );
+    println!("1x0");
     assert_eq!(
         vec![7, 9, 6],
         Solution::spiral_order(vec![vec![7], vec![9], vec![6]])
