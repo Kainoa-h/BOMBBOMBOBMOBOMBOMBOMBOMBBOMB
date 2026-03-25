ea8d6766aab28595957e23706d6027390db48db4
kainoa-h <kainoahowj@gmail.com>
2026-03-26 00:03:55 +0800

    3546: very funky. Mix of strategies. not the best but is hella funk
    time: O(n*m)
    space: O(n*m)

--- rust/src/bin/3546_equal_sum_grid_partition.rs
+++ rust/src/bin/3546_equal_sum_grid_partition.rs
@@ -1,72 +1,48 @@
+use std::ops::ControlFlow;
+
 struct Solution {}
 
 impl Solution {
     pub fn can_partition_grid(grid: Vec<Vec<i32>>) -> bool {
-        let rows = grid.len();
-        println!("grid {:?}", grid);
-        if rows > 1 {
-            let flattened_rows: Vec<u64> = grid
-                .iter()
-                .map(|row| row.iter().map(|&x| x as u64).sum())
-                .collect();
-            let prefix_list = flattened_rows.iter().scan(0, |acc, x| {
-                *acc += *x;
+        let prefix_list: Vec<u64> = grid
+            .iter()
+            .map(|row| row.iter().map(|&x| x as u64).sum::<u64>())
+            .scan(0_u64, |acc, x| {
+                *acc += x;
                 Some(*acc)
-            });
-            let mut postfix_list: Vec<u64> = flattened_rows
-                .iter()
-                .rev()
-                .scan(0_u64, |acc, &x| {
-                    *acc = (*acc).checked_add(x).expect("overflow!");
-                    Some(*acc)
-                })
-                .collect();
-            postfix_list.pop();
-            postfix_list.reverse();
-            postfix_list.push(0);
-            let postfix_list = postfix_list;
+            })
+            .collect();
+        let total: u64 = *prefix_list.last().unwrap();
 
-            for (a, &b) in prefix_list.zip(postfix_list.iter()) {
-                if a == b {
-                    return true;
-                }
-            }
+        if total % 2 == 1 {
+            return false;
         }
 
-        let cols = grid[0].len();
-        if cols > 1 {
-            let flattned_cols: Vec<u64> = (0..cols)
-                .map(|col_idx| grid.iter().map(|row| row[col_idx] as u64).sum())
-                .collect();
-            let prefix_list = flattned_cols.iter().scan(0_u64, |acc, &x| {
-                *acc = (*acc).checked_add(x).expect("overflow!");
-                Some(*acc)
-            });
-            let mut postfix_list: Vec<u64> = flattned_cols
-                .iter()
-                .rev()
-                .scan(0_u64, |acc, x| {
-                    *acc += *x;
-                    Some(*acc)
-                })
-                .collect();
-            postfix_list.pop();
-            postfix_list.reverse();
-            postfix_list.push(0);
-            let postfix_list = postfix_list;
+        let half = total / 2;
 
-            for (a, &b) in prefix_list.zip(postfix_list.iter()) {
-                if a == b {
-                    return true;
-                }
-            }
+        if prefix_list.binary_search(&half).is_ok() {
+            return true;
         }
 
-        false
+        matches!(
+            (0..grid[0].len())
+                .map(|c| grid.iter().map(|rows| rows[c] as u64).sum::<u64>())
+                .try_fold(0_u64, |acc, x| {
+                    let sum = acc + x;
+                    if sum == half {
+                        ControlFlow::Break(true)
+                    } else if sum > half {
+                        ControlFlow::Break(false)
+                    } else {
+                        ControlFlow::Continue(sum)
+                    }
+                }),
+            ControlFlow::Break(true)
+        )
     }
 }
 
 fn main() {
-    // assert!(Solution::can_partition_grid(vec![vec![1, 4], vec![2, 3]]));
+    assert!(Solution::can_partition_grid(vec![vec![1, 4], vec![2, 3]]));
     assert!(!Solution::can_partition_grid(vec![vec![1, 3], vec![2, 4]]));
 }
