a7450f942b6b7151502668f6bf07a3db8b6fbd36
kainoa-h <kainoahowj@gmail.com>
2026-03-27 21:00:36 +0800

    2946
    time: O(n*m)
    space: O(1)

---
+++ rust/src/bin/2946_matrix_similarity_after_cyclic_shifts.rs
@@ -0,0 +1,27 @@
+struct Solution {}
+impl Solution {
+    pub fn are_similar(mat: Vec<Vec<i32>>, k: i32) -> bool {
+        let cols = mat[0].len() as i32;
+        let mut shift = k % cols;
+
+        for row in mat {
+            for i in 0..row.len() {
+                let shift_idx = ((i as i32 + shift).rem_euclid(cols)) as usize;
+                if row[i] != row[shift_idx] {
+                    return false;
+                }
+            }
+            shift *= -1;
+        }
+        true
+    }
+}
+
+fn main() {
+    assert!(Solution::are_similar(vec![vec![1, 2, 3]], 6));
+    assert!(Solution::are_similar(vec![vec![2, 2], vec![2, 2]], 3));
+    assert!(!Solution::are_similar(
+        vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9],],
+        4
+    ));
+}
