887d64cac8222f3a9391ba812e13969ed3d106e2
kainoa-h <kainoahowj@gmail.com>
2026-04-02 21:20:16 +0800

    java 3418: proper solution with dummy col

--- java/src/solutions/3418_maximum_amount_of_money_robot_can_earn.java
+++ java/src/solutions/3418_maximum_amount_of_money_robot_can_earn.java
@@ -1,56 +1,29 @@
+import java.util.Arrays;
+
 class Solution {
   public int maximumAmount(int[][] coins) {
-    final int NEUTRALISES = 2;
-    Integer[][] rollingRow = new Integer[coins[0].length][NEUTRALISES + 1];
-    rollingRow[0][0] = 0;
+    int[][] dp = new int[coins[0].length + 1][3];
+    for (var col : dp) {
+      Arrays.fill(col, Integer.MIN_VALUE / 2);
+    }
+    dp[1][0] = 0;
+    dp[1][1] = 0;
+    dp[1][2] = 0;
 
-    for (int rowIdx = 0; rowIdx < coins.length; rowIdx++) {
-      for (int colIdx = 0; colIdx < coins[0].length; colIdx++) {
-        int coin = coins[rowIdx][colIdx];
-        Integer[] rollingCol = rollingRow[colIdx];
-
-        for (int i = 0; i < NEUTRALISES + 1; i++) {
-          Integer left = null;
-          if (colIdx != 0) {
-            left = rollingRow[colIdx - 1][i];
-          }
-          Integer top = rollingCol[i];
-          if (left == null && top == null) {
-            continue;
-          } else if (left == null) {
-            rollingCol[i] = top + coin;
-          } else if (top == null) {
-            rollingCol[i] = left + coin;
-          } else {
-            rollingCol[i] = Integer.max(top, left) + coin;
-          }
-        }
-
-        if (coin < 0) {
-          var snapshot = rollingCol.clone();
-          for (int i = 0; i < NEUTRALISES; i++) {
-            if (snapshot[i] == null)
-              break;
-            int neuralisedVal = snapshot[i] - coin;
-            if (rollingCol[i + 1] == null || neuralisedVal > rollingCol[i + 1])
-              rollingCol[i + 1] = neuralisedVal;
-          }
-        }
+    for (var row : coins) {
+      for (int colIdx = 1; colIdx < dp.length; colIdx++) {
+        final int coin = row[colIdx - 1];
+        dp[colIdx][2] = Math.max(
+            Math.max(dp[colIdx][2], dp[colIdx - 1][2]) + coin,
+            Math.max(dp[colIdx][1], dp[colIdx - 1][1]));
+        dp[colIdx][1] = Math.max(
+            Math.max(dp[colIdx][1], dp[colIdx - 1][1]) + coin,
+            Math.max(dp[colIdx][0], dp[colIdx - 1][0]));
+        dp[colIdx][0] = Math.max(dp[colIdx][0], dp[colIdx - 1][0]) + coin;
       }
     }
 
-    var last = rollingRow[rollingRow.length - 1];
-    int result = last[0];
-    for (Integer i : last) {
-      if (i == null) {
-        continue;
-      }
-      if (i > result) {
-        result = i;
-      }
-    }
-    return result;
-
+    return dp[dp.length - 1][2];
   }
 
   void main() {
