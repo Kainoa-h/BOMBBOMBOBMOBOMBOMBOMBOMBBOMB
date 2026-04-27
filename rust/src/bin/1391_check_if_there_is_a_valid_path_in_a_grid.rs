57ae00ec6b709d392651e9019e25b59b64220f68
kainoa-h <kainoahowj@gmail.com>
2026-04-28 00:10:24 +0800

    rust 1391: BFS

--- rust/src/bin/1391_check_if_there_is_a_valid_path_in_a_grid.rs
+++ rust/src/bin/1391_check_if_there_is_a_valid_path_in_a_grid.rs
@@ -1,85 +1,63 @@
+use core::panic;
+use std::collections::VecDeque;
+
 struct Solution {}
 
 impl Solution {
-    pub fn has_valid_path(grid: Vec<Vec<i32>>) -> bool {
-        let row_size = grid[0].len();
-        let flat_size = grid.len() * row_size;
-        let mut dsu = DSU::new(flat_size);
-        for (row_idx, row) in grid.iter().enumerate() {
-            for (col_idx, cell) in row.iter().enumerate() {
-                let flatted_curr = row_idx * row_size + col_idx;
-                if Solution::has_top(*cell)
-                    && let Some(x) = row_idx
-                        .checked_sub(1)
-                        .and_then(|i| grid.get(i))
-                        .and_then(|r| r.get(col_idx))
-                    && Solution::has_bot(*x)
-                {
-                    let flattened_top = (row_idx - 1) * row_size + col_idx;
-                    dsu.merge(flattened_top, flatted_curr);
-                }
-                if Solution::has_left(*cell)
-                    && let Some(x) = col_idx.checked_sub(1).and_then(|i| grid[row_idx].get(i))
-                    && Solution::has_right(*x)
-                {
-                    let flattened_left = row_idx * row_size + col_idx - 1;
-                    dsu.merge(flattened_left, flatted_curr);
+    pub fn has_valid_path(mut grid: Vec<Vec<i32>>) -> bool {
+        let mut queue: VecDeque<(usize, usize, (isize, isize))> = VecDeque::new();
+
+        const UP: (isize, isize) = (-1, 0);
+        const RIGHT: (isize, isize) = (0, 1);
+        const DOWN: (isize, isize) = (1, 0);
+        const LEFT: (isize, isize) = (0, -1);
+
+        let get_valid_dirs = |x| match x {
+            1 => [LEFT, RIGHT],
+            2 => [UP, DOWN],
+            3 => [LEFT, DOWN],
+            4 => [RIGHT, DOWN],
+            5 => [UP, LEFT],
+            6 => [UP, RIGHT],
+            _ => panic!(),
+        };
+
+        match grid[0][0] {
+            1 | 6 => queue.push_back((0, 0, RIGHT)),
+            2 | 3 => queue.push_back((0, 0, DOWN)),
+            4 => queue.extend([(0, 0, RIGHT), (0, 0, DOWN)]),
+            _ => queue.push_back((0, 0, UP)),
+        };
+
+        while let Some((r, c, (dr, dc))) = queue.pop_front() {
+            grid[r][c] = -1;
+            if r == grid.len() - 1 && c == grid[0].len() - 1 {
+                return true;
+            }
+            let Some((nr, nc)) = r.checked_add_signed(dr).zip(c.checked_add_signed(dc)) else {
+                continue;
+            };
+            let Some(&next_node) = grid.get(nr).and_then(|row| row.get(nc)) else {
+                continue;
+            };
+            if next_node == -1 {
+                continue;
+            }
+            let receiving_dir = (-dr, -dc);
+            let mut connected = false;
+            let mut next_dir = (0, 0);
+            for n_dir in get_valid_dirs(next_node) {
+                if n_dir == receiving_dir {
+                    connected = true;
+                } else {
+                    next_dir = n_dir;
                 }
             }
+            if connected {
+                queue.push_back((nr, nc, next_dir));
+            }
         }
-        dsu.find(flat_size - 1) == 0
-    }
-
-    fn has_top(street: i32) -> bool {
-        matches!(street, 2 | 5 | 6)
-    }
-
-    fn has_right(street: i32) -> bool {
-        matches!(street, 1 | 4 | 6)
-    }
-    fn has_bot(street: i32) -> bool {
-        matches!(street, 2 | 3 | 4)
-    }
-    fn has_left(street: i32) -> bool {
-        matches!(street, 1 | 3 | 5)
-    }
-}
-
-struct DSU {
-    dsu: Vec<usize>,
-}
-
-impl DSU {
-    fn new(len: usize) -> Self {
-        DSU {
-            dsu: (0..len).collect(),
-        }
-    }
-
-    fn find(&mut self, idx: usize) -> usize {
-        let mut root = self.dsu[idx];
-        while root != self.dsu[root] {
-            root = self.dsu[root];
-        }
-
-        let mut node = idx;
-        while node != root {
-            let next = self.dsu[node];
-            self.dsu[node] = root;
-            node = next;
-        }
-
-        root
-    }
-
-    fn merge(&mut self, a: usize, b: usize) {
-        let a_root = self.find(a);
-        let b_root = self.find(b);
-        if a_root < b_root {
-            self.dsu[b_root] = a_root;
-        } else {
-            self.dsu[a_root] = b_root;
-        }
+        false
     }
 }
 
