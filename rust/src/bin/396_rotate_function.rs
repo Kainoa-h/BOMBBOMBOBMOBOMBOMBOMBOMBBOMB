5d2cdebb215682a485f84a720b05673f67d78348
kainoa-h <kainoahowj@gmail.com>
2026-05-05 20:46:08 +0800

    rust 396: oh right multiplication is just addition...
    time: O(n)
    space: O(1)

--- rust/src/bin/396_rotate_function.rs
+++ rust/src/bin/396_rotate_function.rs
@@ -1,14 +1,17 @@
 struct Solution {}
 impl Solution {
     pub fn max_rotate_function(nums: Vec<i32>) -> i32 {
-        let mut max = i32::MIN;
-        for i in 0..nums.len() {
-            let mut sum = 0;
-            for mult in 1..nums.len() as i32 {
-                let idx = (i + mult as usize) % nums.len();
-                sum += mult * nums[idx];
-            }
-            max = max.max(sum);
+        let incr = nums.iter().sum::<i32>();
+        let mut prev = (1..nums.len()).fold(0, |acc, idx| acc + (idx as i32 * nums[idx]));
+        let mut max = prev;
+
+        // k=0, i=0
+        // k=1, i=3,
+        // k=2, i=2,
+        // k=3, i=1
+        for i in (1..nums.len()).rev() {
+            prev = prev + incr - (nums[i] * nums.len() as i32);
+            max = max.max(prev);
         }
         max
     }
