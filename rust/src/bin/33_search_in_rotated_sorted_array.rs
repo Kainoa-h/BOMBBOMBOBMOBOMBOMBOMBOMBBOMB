5fa2b0affa51194430c81d8c33e685152da67c9d
kainoa-h <kainoahowj@gmail.com>
2026-05-23 16:18:38 +0800

    rust 33: i cheated :(

---
+++ rust/src/bin/33_search_in_rotated_sorted_array.rs
@@ -0,0 +1,36 @@
+struct Solution {}
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut left = 0;
+        let mut right = nums.len() - 1;
+        while left <= right {
+            let mid = left + (right - left) / 2;
+            if nums[mid] == target {
+                return mid as i32;
+            }
+
+            if nums[left] <= nums[mid] {
+                if nums[left] <= target && target < nums[mid] {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if nums[mid] <= target && target < nums[right] {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        -1
+    }
+}
+
+fn main() {
+    assert_eq!(Solution::search(vec![4, 5, 6, 7, 0, 1, 2], 0), 4);
+    assert_eq!(Solution::search(vec![4, 5, 6, 7, 0, 1, 2], 3), -1);
+    assert_eq!(Solution::search(vec![4, 5, 6, 7, 1, 2], 0), -1);
+    assert_eq!(Solution::search(vec![4], 3), -1);
+    assert_eq!(Solution::search(vec![0], 0), 0);
+}
