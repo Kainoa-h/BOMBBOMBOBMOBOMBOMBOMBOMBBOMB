b5f9f790dcc4008d1bb572b722a7511effc65afd
kainoa-h <kainoahowj@gmail.com>
2026-05-30 21:44:36 +0800

    rust 205: slower but more idomatic

--- rust/src/bin/205_isomorphic_strings.rs
+++ rust/src/bin/205_isomorphic_strings.rs
@@ -1,34 +1,32 @@
 struct Solution {}
 impl Solution {
     pub fn is_isomorphic(s: String, t: String) -> bool {
-        let mut map = [-1_i16; 128];
-        let mut u_map = [false; 128];
-        for i in 0..s.len() {
-            let c1 = s.as_bytes()[i];
-            let c2 = t.as_bytes()[i];
-            let x = map.get_mut(c1 as usize).unwrap();
-            let y = u_map.get_mut(c2 as usize).unwrap();
+        let mut s_map = [None::<u8>; 128];
+        let mut t_check = [None::<u8>; 128];
 
-            if *x == -1 && !*y {
-                *x = c2 as i16;
-                *y = true;
-            } else if *x == -1 && *y {
+        for (c1, c2) in s.bytes().zip(t.bytes()) {
+            let e1 = s_map.get_mut(c1 as usize).unwrap().get_or_insert(c2);
+            if *e1 != c2 {
                 return false;
-            } else if *x != c2 as i16 {
+            }
+
+            let e2 = t_check.get_mut(c2 as usize).unwrap().get_or_insert(c1);
+            if *e2 != c1 {
                 return false;
             }
         }
+
         true
     }
 }
 
 fn main() {
-    // assert!(Solution::is_isomorphic("egg".to_owned(), "add".to_owned()));
-    // assert!(!Solution::is_isomorphic("f11".to_owned(), "b23".to_owned()));
-    // assert!(Solution::is_isomorphic(
-    //     "paper".to_owned(),
-    //     "title".to_owned()
-    // ));
+    assert!(Solution::is_isomorphic("egg".to_owned(), "add".to_owned()));
+    assert!(!Solution::is_isomorphic("f11".to_owned(), "b23".to_owned()));
+    assert!(Solution::is_isomorphic(
+        "paper".to_owned(),
+        "title".to_owned()
+    ));
     assert!(!Solution::is_isomorphic(
         "badc".to_owned(),
         "baba".to_owned()
