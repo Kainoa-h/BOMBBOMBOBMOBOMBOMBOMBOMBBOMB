866d2132509c8a4d5ea09d9e3f3c8701afcafc91
kainoa-h <kainoahowj@gmail.com>
2026-06-01 21:52:05 +0800

    rust 2144: functional approach

--- rust/src/bin/2144_minimum_cost_of_buying_candies_with_discount.rs
+++ rust/src/bin/2144_minimum_cost_of_buying_candies_with_discount.rs
@@ -2,18 +2,7 @@ struct Solution {}
 impl Solution {
     pub fn minimum_cost(mut cost: Vec<i32>) -> i32 {
         cost.sort_unstable_by(|a, b| b.cmp(a));
-
-        let mut min_cost = 0;
-        let mut state = 0;
-        for c in cost {
-            if state == 2 {
-                state = 0;
-                continue;
-            }
-            min_cost += c;
-            state += 1;
-        }
-        min_cost
+        cost.chunks(3).map(|c| c.iter().take(2).sum::<i32>()).sum()
     }
 }
 
