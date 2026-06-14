e1c66792016c91bec854dceee1b323ee947929c8
kainoa-h <kainoahowj@gmail.com>
2026-06-14 09:07:54 +0800

    rust 2130: reverse pointer

--- rust/src/bin/2130_maximum_twin_sum_of_a_linked_list.rs
+++ rust/src/bin/2130_maximum_twin_sum_of_a_linked_list.rs
@@ -12,16 +12,35 @@ impl ListNode {
     }
 }
 impl Solution {
-    pub fn pair_sum(mut head: Option<Box<ListNode>>) -> i32 {
-        let mut vec = Vec::new();
-        while let Some(node) = head {
-            head = node.next;
-            vec.push(node.val);
+    pub fn pair_sum(head: Option<Box<ListNode>>) -> i32 {
+        let mut fast_head = &head;
+        let mut flipper_head = &head;
+        while let Some(flipper_node) = flipper_head
+            && let Some(fast_node) = fast_head
+        {
+            flipper_head = &flipper_node.next;
+            fast_head = &fast_node.next.as_ref().unwrap().next;
         }
+
+        let mut flipper_head = flipper_head.clone();
+        let mut prev_node = None;
+        while let Some(mut node) = flipper_head {
+            flipper_head = node.next;
+            node.next = prev_node;
+            prev_node = Some(node);
+        }
+
+        let mut head_head = &head;
+        let mut rev_head = &prev_node;
         let mut max = 0;
-        for i in 0..(vec.len() / 2) {
-            max = max.max(vec[i] + vec[vec.len() - 1 - i]);
+        while let Some(head_node) = &head_head
+            && let Some(rev_node) = &rev_head
+        {
+            max = max.max(head_node.val + rev_node.val);
+            head_head = &head_node.next;
+            rev_head = &rev_node.next;
         }
+
         max
     }
 }
