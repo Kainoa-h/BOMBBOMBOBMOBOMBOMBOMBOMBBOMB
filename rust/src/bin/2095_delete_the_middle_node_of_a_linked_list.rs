efb7d4778f421f2a5839fa6f3eb04ae7d70d9711
kainoa-h <kainoahowj@gmail.com>
2026-06-15 16:14:18 +0800

    rust 2095: ownership confusion
    fast pointer approach doesn't work here since I can't clone

---
+++ rust/src/bin/2095_delete_the_middle_node_of_a_linked_list.rs
@@ -0,0 +1,45 @@
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+impl Solution {
+    pub fn delete_middle(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut fast_head = head.as_ref();
+        let mut middle_index = 0;
+        while let Some(fast_node) = fast_head {
+            fast_head = fast_node.next.as_ref();
+            if fast_head.is_some() {
+                middle_index += 1;
+            }
+            fast_head = fast_head.and_then(|x| x.next.as_ref());
+        }
+
+        if middle_index == 0 {
+            return None;
+        }
+
+        let mut bef_middle_head = head.as_mut();
+        for _ in 0..middle_index - 1 {
+            bef_middle_head = bef_middle_head.unwrap().next.as_mut();
+        }
+
+        if let Some(bef_mid_node) = bef_middle_head {
+            let middle_head = bef_mid_node.next.take();
+            let mut mid_node = middle_head?;
+            bef_mid_node.next = mid_node.next.take();
+        }
+
+        head
+    }
+}
+
+struct Solution {}
