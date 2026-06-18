540d877a8905c04b7a0a94e614e75f1069d7e5da
kainoa-h <kainoahowj@gmail.com>
2026-06-18 15:08:04 +0800

    rust 1344: simplified

--- rust/src/bin/1344_angle_between_hands_of_a_clock.rs
+++ rust/src/bin/1344_angle_between_hands_of_a_clock.rs
@@ -4,10 +4,8 @@ static MIN_ANGLE_INCR: f64 = 6.0;
 
 impl Solution {
     pub fn angle_clock(hour: i32, minutes: i32) -> f64 {
-        let hour = if hour == 12 { 0.0 } else { hour as f64 };
-        let minutes = if minutes == 60 { 0.0 } else { minutes as f64 };
-        let hour_angle = (hour * HOUR_ANGLE_INCR) + (minutes * SUB_HOUR_ANGLE_INCR);
-        let min_angle = minutes * MIN_ANGLE_INCR;
+        let hour_angle = (hour as f64 * HOUR_ANGLE_INCR) + (minutes as f64 * SUB_HOUR_ANGLE_INCR);
+        let min_angle = minutes as f64 * MIN_ANGLE_INCR;
         let diff = (hour_angle - min_angle).abs();
         diff.min(360.0 - diff)
     }
