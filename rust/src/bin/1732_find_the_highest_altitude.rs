5a2a3059847707dff898f48963a4319ac5d73d52
kainoa-h <kainoahowj@gmail.com>
2026-06-19 08:57:38 +0800

    rust 1732: functional-ish

--- rust/src/bin/1732_find_the_highest_altitude.rs
+++ rust/src/bin/1732_find_the_highest_altitude.rs
@@ -1,13 +1,13 @@
 impl Solution {
     pub fn largest_altitude(gain: Vec<i32>) -> i32 {
-        let mut alt = 0;
-        let mut max_alt = 0;
-        for g in gain {
-            alt += g;
-            max_alt = max_alt.max(alt);
-        }
-
-        max_alt
+        gain.iter()
+            .scan(0, |acc, &x| {
+                *acc += x;
+                Some(*acc)
+            })
+            .max()
+            .unwrap()
+            .max(0)
     }
 }
 
