2a2555b2de70550743eaa166cfb3d1e408d1b638
kainoa-h <kainoahowj@gmail.com>
2026-06-19 09:14:56 +0800

    rust 228: cleanup

--- rust/src/bin/228_summary_ranges.rs
+++ rust/src/bin/228_summary_ranges.rs
@@ -7,30 +7,27 @@ impl Solution {
         let mut prev = start;
 
         for &n in nums.iter().skip(1) {
-            if n == prev {
-                continue;
-            }
-
-            if prev + 1 == n {
+            if n == prev || prev + 1 == n {
                 prev = n;
                 continue;
             }
 
-            let mut range = start.to_string();
-            if start != prev {
-                range.push_str("->");
-                range.push_str(&prev.to_string());
-            }
+            result.push(if start != prev {
+                format!("{}->{}", start, prev)
+            } else {
+                start.to_string()
+            });
+
             prev = n;
             start = n;
-            result.push(range);
         }
-        let mut range = start.to_string();
-        if start != prev {
-            range.push_str("->");
-            range.push_str(&prev.to_string());
-        }
-        result.push(range);
+
+        result.push(if start != prev {
+            format!("{}->{}", start, prev)
+        } else {
+            start.to_string()
+        });
+
         result
     }
 }
