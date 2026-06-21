e6583c06ac6793c0baaf1b65c1466c19310eab45
kainoa-h <kainoahowj@gmail.com>
2026-06-21 10:20:58 +0800

    rust 1840: one less interation

--- rust/src/bin/1840_maximum_building_height.rs
+++ rust/src/bin/1840_maximum_building_height.rs
@@ -1,15 +1,8 @@
 impl Solution {
     pub fn max_building(n: i32, mut restrictions: Vec<Vec<i32>>) -> i32 {
         restrictions.push(vec![1, 0]);
-        restrictions.push(vec![n, n - 1]);
         restrictions.sort_unstable_by_key(|x| x[0]);
-
-        for idx in 0..restrictions.len() - 1 {
-            let (p_idx, p_max) = (restrictions[idx][0], restrictions[idx][1]);
-            let (n_idx, n_max) = (restrictions[idx + 1][0], &mut restrictions[idx + 1][1]);
-            let dist = n_idx - p_idx;
-            *n_max = (p_max + dist).min(*n_max);
-        }
+        restrictions.push(vec![n, n - 1]);
 
         for idx in (1..restrictions.len()).rev() {
             let (p_idx, p_max) = (restrictions[idx][0], restrictions[idx][1]);
@@ -20,11 +13,12 @@ impl Solution {
 
         let mut max = 0;
 
-        for win in restrictions.windows(2) {
-            let (p_idx, p_max) = (win[0][0], win[0][1]);
-            let (n_idx, n_max) = (win[1][0], win[1][1]);
+        for idx in 0..restrictions.len() - 1 {
+            let (p_idx, p_max) = (restrictions[idx][0], restrictions[idx][1]);
+            let (n_idx, n_max) = (restrictions[idx + 1][0], &mut restrictions[idx + 1][1]);
             let dist = n_idx - p_idx;
-            max = max.max((p_max + n_max + dist) / 2);
+            *n_max = (p_max + dist).min(*n_max);
+            max = max.max((p_max + *n_max + dist) / 2);
         }
 
         max
