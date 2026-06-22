bb3894e9d2b39eefbc55b9d92f14632f3218c134
kainoa-h <kainoahowj@gmail.com>
2026-06-22 21:32:00 +0800

    rust 1833: counting sort!

--- rust/src/bin/1833_maximum_icecream_bars.rs
+++ rust/src/bin/1833_maximum_icecream_bars.rs
@@ -1,16 +1,28 @@
 impl Solution {
-    pub fn max_ice_cream(mut costs: Vec<i32>, mut coins: i32) -> i32 {
-        costs.sort_unstable();
-        let mut bought = 0;
-        for price in costs {
-            if price > coins {
-                break;
-            }
-            bought += 1;
-            coins -= price;
+    pub fn max_ice_cream(costs: Vec<i32>, coins: i32) -> i32 {
+        let mut coins = coins as usize;
+        let mut counting = [0; 100_001];
+        for cost in costs {
+            counting[cost as usize] += 1;
         }
-        bought
+
+        let mut count = 0;
+        for (cost, stock) in counting.iter_mut().enumerate().skip(1) {
+            while *stock > 0 {
+                if coins < cost {
+                    return count;
+                }
+                *stock -= 1;
+                coins -= cost;
+                count += 1;
+            }
+        }
+        count
     }
 }
 
 struct Solution {}
+
+fn main() {
+    assert_eq!(Solution::max_ice_cream(vec![1, 3, 2, 4, 1], 7), 4);
+}
