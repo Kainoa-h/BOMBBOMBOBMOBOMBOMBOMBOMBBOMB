f84952d94d344ee1b5eb6afefc38b8fb54db18f9
kainoa-h <kainoahowj@gmail.com>
2026-06-30 15:03:45 +0800

    rust 1358: editorial

--- rust/src/bin/1358_number_of_substrings_containing_all_three_characters.rs
+++ rust/src/bin/1358_number_of_substrings_containing_all_three_characters.rs
@@ -1,46 +1,12 @@
-use std::collections::LinkedList;
 impl Solution {
     pub fn number_of_substrings(s: String) -> i32 {
-        let mut a_ll = LinkedList::<usize>::new();
-        let mut b_ll = LinkedList::<usize>::new();
-        let mut c_ll = LinkedList::<usize>::new();
-        for (i, b) in s.bytes().enumerate() {
-            match b {
-                b'a' => a_ll.push_back(i),
-                b'b' => b_ll.push_back(i),
-                b'c' => c_ll.push_back(i),
-                _ => {}
-            }
-        }
-
         let mut count = 0;
-
+        let mut seen = vec![-1; 3];
         for (i, b) in s.bytes().enumerate() {
-            if a_ll.is_empty() || b_ll.is_empty() || c_ll.is_empty() {
-                break;
+            seen[(b - b'a') as usize] = i as i32;
+            if seen[0] != -1 && seen[1] != -1 && seen[2] != -1 {
+                count += 1 + seen[0].min(seen[1]).min(seen[2]);
             }
-            let (a, b, c) = match b {
-                b'a' => (
-                    a_ll.pop_front().unwrap(),
-                    *b_ll.front().unwrap(),
-                    *c_ll.front().unwrap(),
-                ),
-                b'b' => (
-                    *a_ll.front().unwrap(),
-                    b_ll.pop_front().unwrap(),
-                    *c_ll.front().unwrap(),
-                ),
-                b'c' => (
-                    *a_ll.front().unwrap(),
-                    *b_ll.front().unwrap(),
-                    c_ll.pop_front().unwrap(),
-                ),
-                _ => panic!(),
-            };
-
-            let max = a.max(b).max(c);
-            let slices = s.len() - max;
-            count += slices as i32;
         }
 
         count
