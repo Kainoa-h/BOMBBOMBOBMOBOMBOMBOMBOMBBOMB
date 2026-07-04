8c4e955a159d24f7d84c7b2e661a85a55f4c3d3d
kainoa-h <kainoahowj@gmail.com>
2026-07-04 22:04:42 +0800

    rust 2492: cursed dsu

---
+++ rust/src/bin/2492_minimum_score_of_a_path_between_two_cities.rs
@@ -0,0 +1,93 @@
+impl Solution {
+    pub fn min_score(n: i32, roads: Vec<Vec<i32>>) -> i32 {
+        let mut dsu = DSU::new(n as usize + 1);
+        for r in roads {
+            let (a, b, d) = (r[0], r[1], r[2]);
+            dsu.merge(a as usize, b as usize, d);
+        }
+        dsu.flatten();
+        dsu.dsu[1].1
+    }
+}
+
+struct DSU {
+    dsu: Vec<(usize, i32)>,
+}
+
+impl DSU {
+    fn new(dsu: usize) -> Self {
+        Self {
+            dsu: (0..dsu).map(|x| (x, i32::MAX)).collect(),
+        }
+    }
+
+    fn find(&mut self, idx: usize, dist: i32) -> (usize, i32) {
+        let (i, d) = self.dsu[idx];
+        if i == idx {
+            self.dsu[i].1 = d.min(dist);
+            return (i, self.dsu[i].1);
+        }
+        self.dsu[idx] = self.find(i, dist.min(d));
+        self.dsu[idx]
+    }
+
+    fn merge(&mut self, a: usize, b: usize, d: i32) {
+        let (root_a, dist_a) = self.find(a, d);
+        let (root_b, dist_b) = self.find(b, d);
+        if root_a != root_b {
+            let (parent, child) = (root_a.min(root_b), root_a.max(root_b));
+            self.dsu[child] = (parent, dist_a.min(dist_b))
+        }
+    }
+
+    fn flatten(&mut self) {
+        for i in 0..self.dsu.len() {
+            self.find(i, i32::MAX);
+        }
+    }
+}
+
+struct Solution {}
+
+fn main() {
+    assert_eq!(
+        Solution::min_score(4, vec![vec![1, 2, 2], vec![1, 3, 4], vec![3, 4, 7]]),
+        2
+    );
+
+    assert_eq!(
+        Solution::min_score(
+            13,
+            vec![
+                vec![2, 12, 1891],
+                vec![10, 9, 4138],
+                vec![11, 3, 2007],
+                vec![1, 10, 9390],
+                vec![12, 8, 1915],
+                vec![6, 2, 1098],
+                vec![5, 4, 2795],
+                vec![3, 13, 4562],
+                vec![9, 7, 9202],
+                vec![4, 6, 6752],
+                vec![8, 11, 1480],
+                vec![7, 5, 9827]
+            ]
+        ),
+        1098
+    );
+
+    assert_eq!(
+        Solution::min_score(
+            7,
+            vec![
+                vec![1, 3, 1484],
+                vec![3, 2, 3876],
+                vec![2, 4, 6823],
+                vec![6, 7, 579],
+                vec![5, 6, 4436],
+                vec![4, 5, 8830]
+            ]
+        ),
+        579
+    );
+}
