a86e237f75b720cc1aea59d4dcdf4b1446f0d54a
kainoa-h <kainoahowj@gmail.com>
2026-07-13 09:58:14 +0800

    rust 1291: O(1) solution lol

--- rust/src/bin/1291_sequential_digits.rs
+++ rust/src/bin/1291_sequential_digits.rs
@@ -1,47 +1,14 @@
 impl Solution {
     pub fn sequential_digits(low: i32, high: i32) -> Vec<i32> {
-        let mut result = Vec::new();
-
-        let (low_len, high_len) = (low.ilog10() as i32 + 1, high.ilog10() as i32 + 1);
-
-        fn most_significant_digit_log(n: i32) -> i32 {
-            if n == 0 {
-                return 0;
-            }
-            let log = (n as f64).log10().floor() as u32;
-            n / 10_i32.pow(log)
-        }
-
-        let mut len = low_len;
-        let mut start = most_significant_digit_log(low);
-        loop {
-            if start + len > 10 {
-                start = 1;
-                len += 1;
-            }
-
-            if len > 9 {
-                break;
-            }
-
-            let candidate = (start..start + len)
-                .rev()
-                .enumerate()
-                .fold(0, |acc, (idx, x)| acc + (x * 10_i32.pow(idx as u32)));
-
-            if candidate < low {
-                start += 1;
-                continue;
-            }
-
-            if len < high_len || candidate <= high {
-                result.push(candidate);
-                start += 1;
-            } else {
-                break;
-            }
-        }
-        result
+        [
+            12_i32, 23, 34, 45, 56, 67, 78, 89,
+            123, 234, 345, 456, 567, 678, 789,
+            1234, 2345, 3456, 4567, 5678, 6789,
+            12345, 23456, 34567, 45678, 56789, 123456, 234567, 345678, 456789,
+            1234567, 2345678, 3456789, 12345678, 23456789, 123456789,
+        ].into_iter().filter(|&x|{
+            low <= x && x <= high
+        }).collect()
     }
 }
 
