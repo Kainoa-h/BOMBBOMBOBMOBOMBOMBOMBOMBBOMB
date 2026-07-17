157b0a4fd103a16bfb959a209910d990cffe0985
Kainoa-h <kainoahowj@gmail.com>
2026-07-17 15:33:07 +0800

    rust 155: less efficient, but simplier

--- rust/src/bin/155_min_stack.rs
+++ rust/src/bin/155_min_stack.rs
@@ -1,39 +1,32 @@
 struct MinStack {
-    stack: Vec<i32>,
-    min: Vec<i32>,
+    stack: Vec<(i32,i32)>
 }
 
 impl MinStack {
     fn new() -> Self {
         Self {
             stack: Vec::new(),
-            min: Vec::new(),
         }
     }
 
     fn push(&mut self, value: i32) {
-        self.stack.push(value);
-        if let Some(&min) = self.min.last() {
-            if value <= min {
-                self.min.push(value);
-            }
-        } else {
-            self.min.push(value);
-        }
+        let min = match self.stack.last() {
+            Some(m) => m.1.min(value),
+            None => value
+        };
+        self.stack.push((value, min));
     }
 
     fn pop(&mut self) {
-        if let Some(v) = self.stack.pop() && let Some(&m) = self.min.last() && m == v {
-            self.min.pop();
-        }
+        self.stack.pop();
     }
 
     fn top(&self) -> i32 {
-        *self.stack.last().unwrap()
+        self.stack.last().unwrap().0
     }
 
     fn get_min(&self) -> i32 {
-        *self.min.last().unwrap()
+        self.stack.last().unwrap().1
     }
 }
 
