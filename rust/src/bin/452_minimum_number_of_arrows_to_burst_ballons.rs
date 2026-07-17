92793fa3db58831d1c339a7da05afeab4da6fc0e
Kainoa-h <kainoahowj@gmail.com>
2026-07-17 11:24:11 +0800

    rust 452: simplified

--- rust/src/bin/452_minimum_number_of_arrows_to_burst_ballons.rs
+++ rust/src/bin/452_minimum_number_of_arrows_to_burst_ballons.rs
@@ -1,14 +1,9 @@
 impl Solution {
-    pub fn find_min_arrow_shots(points: Vec<Vec<i32>>) -> i32 {
-        let mut points = points
-            .iter()
-            .map(|v| [v[0] as i64, v[1] as i64])
-            .collect::<Vec<[i64; 2]>>();
+    pub fn find_min_arrow_shots(mut points: Vec<Vec<i32>>) -> i32 {
         points.sort_unstable_by_key(|v| (v[0], v[1]));
-        points.push([i64::MAX, 0]);
 
         let mut overlap_end = points[0][1];
-        let mut result = 0;
+        let mut result = 1;
         for v in points.iter().skip(1) {
             let (start, end) = (v[0], v[1]);
             overlap_end = overlap_end.min(end);
