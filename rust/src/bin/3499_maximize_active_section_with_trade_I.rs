21637e56b762af280907625cd2a86206e441563d
kainoa-h <kainoahowj@gmail.com>
2026-07-21 09:12:26 +0800

    rust 3499: cool use of chunk_by!

--- rust/src/bin/3499_maximize_active_section_with_trade_I.rs
+++ rust/src/bin/3499_maximize_active_section_with_trade_I.rs
@@ -1,32 +1,20 @@
 impl Solution {
     pub fn max_active_sections_after_trade(s: String) -> i32 {
-        let mut compressed = Vec::new();
-        let mut count = 1;
-        let s = format!("{};", s);
-        for w in s.as_bytes().windows(2) {
-            let (prev, now) = (w[0], w[1]);
-            if prev == now {
-                count += 1;
-            } else {
-                compressed.push((prev, count));
-                count = 1;
-            }
-        }
-
-        let mut max = 0;
-        for w in compressed.windows(3) {
-            if w[0].0 != b'0' {
+        let mut total_ones = 0;
+        let mut max_delta = 0;
+        let mut prev_zeros = 0;
+        for chunk in s.as_bytes().chunk_by(|a, b| a == b) {
+            let len = chunk.len();
+            if chunk[0] == b'1' {
+                total_ones += len;
                 continue;
             }
-            let delta = w[0].1 + w[2].1;
-            max = max.max(delta);
+            if prev_zeros != 0 {
+                max_delta = max_delta.max(len + prev_zeros);
+            }
+            prev_zeros = len;
         }
-
-        max + compressed
-            .into_iter()
-            .filter(|x| x.0 == b'1')
-            .map(|x| x.1)
-            .sum::<i32>()
+        (total_ones + max_delta) as i32
     }
 }
 
