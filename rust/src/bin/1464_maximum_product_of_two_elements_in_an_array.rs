94af922e2e553bc62db1d60aad2f443da77982bb
Kainoa-h <kainoahowj@gmail.com>
2026-07-27 11:20:57 +0800

    rust 1464: functional for fun

--- rust/src/bin/1464_maximum_product_of_two_elements_in_an_array.rs
+++ rust/src/bin/1464_maximum_product_of_two_elements_in_an_array.rs
@@ -1,11 +1,10 @@
 impl Solution {
     pub fn max_product(nums: Vec<i32>) -> i32 {
-        let mut first = 0;
-        let mut second = 0;
-        for n in nums {
-            second = second.max(first.min(n));
-            first = first.max(n);
-        }
+        let (first, second) = nums.iter().fold((0,0), |mut acc, &n| {
+            acc.1 = acc.1.max(acc.0.min(n));
+            acc.0 = acc.0.max(n);
+            acc
+        });
         (first - 1) * (second - 1)
     }
 }
