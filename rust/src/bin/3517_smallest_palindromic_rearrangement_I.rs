2a0fe2f592f4ac1a8ef84403485acc11d5e2e9b4
kainoa-h <kainoahowj@gmail.com>
2026-07-28 10:58:50 +0800

    rust 3517: much slower due to sort, but so clean and with less allocs!
    also has no extra allocations other than during the sort.
    - into_bytes reuses the original string allocation
    - so does the final vec to string pointer cast

--- rust/src/bin/3517_smallest_palindromic_rearrangement_I.rs
+++ rust/src/bin/3517_smallest_palindromic_rearrangement_I.rs
@@ -1,26 +1,14 @@
 impl Solution {
     pub fn smallest_palindrome(s: String) -> String {
-        let mut count = [0; 26];
-        for &c in s.as_bytes() {
-            count[(c - b'a') as usize] += 1;
+        let len = s.len();
+        let mid = len / 2;
+        let mut result = s.into_bytes();
+        result[0..mid].sort_unstable();
+        for i in 0..mid {
+            result[len - i - 1] = result[i];
         }
 
-        let mut left_half = Vec::<u8>::with_capacity(s.len()/2);
-        for (i, count) in count.iter().enumerate() {
-            let char = i as u8 + b'a';
-            left_half.extend(std::iter::repeat_n(char, count/2));
-        }
-
-        let mut result = Vec::with_capacity(s.len());
-        result.extend(&left_half);
-        if s.len() % 2 != 0 {
-            result.push(s.as_bytes()[s.len()/2]);
-        }
-        result.extend(left_half.iter().rev().copied());
-
-        unsafe {
-            String::from_utf8_unchecked(result)
-        }
+        unsafe { String::from_utf8_unchecked(result) }
     }
 }
 
