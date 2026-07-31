a015e25b81d99656244044891a78254009f15520
kainoa-h <kainoahowj@gmail.com>
2026-07-31 10:13:24 +0800

    rust 3016: chunk!

--- rust/src/bin/3016_minimum_number_of_pushes_to_type_word_II.rs
+++ rust/src/bin/3016_minimum_number_of_pushes_to_type_word_II.rs
@@ -1,18 +1,15 @@
 impl Solution {
     pub fn minimum_pushes(word: String) -> i32 {
-        let mut bucket = vec![0; 26];
+        let mut bucket = [0; 26];
         for c in word.into_bytes() {
             bucket[(c - b'a') as usize] += 1;
         }
         bucket.sort_unstable_by_key(|x| -x);
-        bucket.into_iter().enumerate().fold(0, |acc, (i, n)|{
-            acc + n * match i {
-                i if i < 8 => 1,
-                i if i < 16 => 2,
-                i if i < 24 => 3,
-                _ => 4,
-            }
-        })
+        bucket
+            .chunks(8)
+            .enumerate()
+            .map(|(i, chunk)| chunk.iter().sum::<i32>() * (i as i32 + 1))
+            .sum()
     }
 }
 
