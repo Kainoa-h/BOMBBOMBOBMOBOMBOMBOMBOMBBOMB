8e3ae8a82d76bd197fcab98d629db24e827dcbc1
Kainoa-h <kainoahowj@gmail.com>
2026-08-05 21:52:03 +0800

    rust 2: fold last carry into the while loop

--- rust/src/bin/2_add_two_numbers.rs
+++ rust/src/bin/2_add_two_numbers.rs
@@ -17,25 +17,19 @@ impl Solution {
         mut l2: Option<Box<ListNode>>,
     ) -> Option<Box<ListNode>> {
         let mut carry = 0;
-        let mut head = ListNode {
-            val: 0, next: None
-        };
+        let mut head = ListNode::new(0);
         let mut node = &mut head;
-        while l1.is_some() || l2.is_some() {
-            let n1 = l1.as_ref().map_or(0, |x|x.val);
-            let n2 = l2.as_ref().map_or(0, |x|x.val);
+        while l1.is_some() || l2.is_some() || carry != 0 {
+            let n1 = l1.as_ref().map_or(0, |x| x.val);
+            let n2 = l2.as_ref().map_or(0, |x| x.val);
             let sum = n1 + n2 + carry;
-            let digit = sum % 10;
-
-            node.next = Some(Box::new(ListNode{ val: digit, next: None}));
-            node = node.next.as_mut().unwrap();
-
             carry = sum / 10;
-            l1 = l1.map_or(None, |x| x.next);
-            l2 = l2.map_or(None, |x| x.next);
-        }
-        if carry != 0 {
-            node.next = Some(Box::new(ListNode{ val: carry, next: None}));
+
+            l1 = l1.and_then(|x| x.next);
+            l2 = l2.and_then(|x| x.next);
+
+            node.next = Some(Box::new(ListNode::new(sum % 10)));
+            node = node.next.as_mut().unwrap();
         }
         head.next
     }
