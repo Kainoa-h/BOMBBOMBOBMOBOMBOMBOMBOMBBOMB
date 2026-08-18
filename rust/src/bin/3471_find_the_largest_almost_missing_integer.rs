d429e5dec7278fc4fae2792a8ed998f8d2e25a09
Kainoa-h <kainoahowj@gmail.com>
2026-08-18 13:41:32 +0800

    rust 3471: early escape with no max tracking for case 1

--- rust/src/bin/3471_find_the_largest_almost_missing_integer.rs
+++ rust/src/bin/3471_find_the_largest_almost_missing_integer.rs
@@ -1,23 +1,28 @@
 impl Solution {
     pub fn largest_integer(nums: Vec<i32>, k: i32) -> i32 {
+        let len = nums.len();
         if k == 1 {
             let mut bucket = [0; 51];
             for &n in &nums {
                 bucket[n as usize] += 1;
             }
-            nums.into_iter()
-                .filter(|&x| bucket[x as usize] == 1)
-                .max()
+            bucket
+                .into_iter()
+                .enumerate()
+                .rev()
+                .find_map(|(n, c)|
+                    (c == 1).then_some(n as i32)
+                )
                 .unwrap_or(-1)
-        } else if k == nums.len() as i32 {
+        } else if k == len as i32 {
             nums.into_iter().max().unwrap_or(-1)
         } else {
             let first = nums[0];
-            let last = *nums.last().unwrap();
+            let last = nums[len-1];
             if first == last {
                 return -1;
             }
-            let (seen_first, seen_last) = nums[1..nums.len()-1].iter().fold((false, false), |(sf, sl), &x|{
+            let (seen_first, seen_last) = nums[1..len - 1].iter().fold((false, false), |(sf, sl), &x| {
                 (sf || x == first, sl || x == last)
             });
 
