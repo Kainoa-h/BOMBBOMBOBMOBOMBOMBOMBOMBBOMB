2c48aa46e2d5c64b95909fd4941aeccc956d7c9d
Kainoa-h <kainoahowj@gmail.com>
2026-08-18 13:29:53 +0800

    java 3471: better code formatting and early escape with no max tracking for case 1

--- java/src/solutions/3471_find_the_largest_almost_missing_integer.java
+++ java/src/solutions/3471_find_the_largest_almost_missing_integer.java
@@ -1,41 +1,40 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
+        var len = nums.length;
         if (k == 1) {
             var bucket = new int[51];
             for (int n : nums) {
                 bucket[n]++;
             }
-            int max = -1;
-            for (int i = 0; i < bucket.length; i++) {
+            for (int i = 50; i > 0; i--) {
                 if (bucket[i] == 1) {
-                    max = Integer.max(max, i);
+                    return i;
                 }
             }
-            return max;
-        } else if (k == nums.length) {
+            return -1;
+        }
+        if (k == len) {
             int max = -1;
             for (int n : nums) {
                 max = Integer.max(max, n);
             }
             return max;
-        } else {
-            var first = nums[0];
-            var last = nums[nums.length - 1];
-            if (first == last) {
-                return -1;
-            }
-            var is_first = true;
-            var is_last = true;
-            for (int i = 1; i < nums.length - 1; i++) {
-                if (nums[i] == first) {
-                    is_first = false;
-                } else if (nums[i] == last) {
-                    is_last = false;
-                }
-            }
-            first = is_first ? first : -1;
-            last = is_last ? last : -1;
-            return Integer.max(first, last);
         }
+
+        var first = nums[0];
+        var last = nums[len - 1];
+        if (first == last) {
+            return -1;
+        }
+        var seen_first = false;
+        var seen_last = false;
+        for (int i = 1; i < len - 1; i++) {
+            if (nums[i] == first) seen_first = true;
+            else if (nums[i] == last) seen_last = true;
+            if (seen_first && seen_last) return -1;
+        }
+        first = seen_first ? -1 : first;
+        last = seen_last ? -1 : last;
+        return Integer.max(first, last);
     }
 }
