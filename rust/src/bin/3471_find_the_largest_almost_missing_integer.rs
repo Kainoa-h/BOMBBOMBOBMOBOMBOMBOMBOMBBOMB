f8bb44de4209cc3da270c3d7069a3447d281cb23
Kainoa-h <kainoahowj@gmail.com>
2026-08-18 13:15:38 +0800

    rust 3471: simplified code with better use of iterators

--- rust/src/bin/3471_find_the_largest_almost_missing_integer.rs
+++ rust/src/bin/3471_find_the_largest_almost_missing_integer.rs
@@ -2,39 +2,28 @@ impl Solution {
     pub fn largest_integer(nums: Vec<i32>, k: i32) -> i32 {
         if k == 1 {
             let mut bucket = [0; 51];
-            for n in nums {
+            for &n in &nums {
                 bucket[n as usize] += 1;
             }
-            bucket
-                .iter()
-                .enumerate()
-                .filter(|&(_, &c)| c == 1)
-                .map(|(n, _)| n as i32)
+            nums.into_iter()
+                .filter(|&x| bucket[x as usize] == 1)
                 .max()
                 .unwrap_or(-1)
         } else if k == nums.len() as i32 {
-            *nums.iter().max().unwrap_or(&-1)
+            nums.into_iter().max().unwrap_or(-1)
         } else {
             let first = nums[0];
             let last = *nums.last().unwrap();
             if first == last {
-                -1
-            } else {
-                let mut is_first = true;
-                let mut is_last = true;
-
-                for &n in &nums[1..nums.len() - 1] {
-                    if n == first {
-                        is_first = false;
-                    } else if n == last {
-                        is_last = false;
-                    }
-                }
-
-                let first = if is_first { first } else { -1 };
-                let last = if is_last { last } else { -1 };
-                first.max(last)
+                return -1;
             }
+            let (seen_first, seen_last) = nums[1..nums.len()-1].iter().fold((false, false), |(sf, sl), &x|{
+                (sf || x == first, sl || x == last)
+            });
+
+            let first = if seen_first { -1 } else { first };
+            let last = if seen_last { -1 } else { last };
+            first.max(last)
         }
     }
 }
