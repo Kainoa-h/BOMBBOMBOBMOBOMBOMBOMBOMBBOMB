e7c519ff5c8929c8295a0c62dfd4add4476a0e23
Kainoa-h <kainoahowj@gmail.com>
2026-08-19 13:49:43 +0800

    rust 1386: sort and chunk

--- rust/src/bin/1386_cinema_seat_allocation.rs
+++ rust/src/bin/1386_cinema_seat_allocation.rs
@@ -1,13 +1,13 @@
 impl Solution {
-    pub fn max_number_of_families(n: i32, reserved_seats: Vec<Vec<i32>>) -> i32 {
-        let mut seats = vec![vec![true; 10]; n as usize];
-        for v in reserved_seats {
-            let (row, col) = (v[0] as usize - 1, v[1] as usize - 1);
-            seats[row][col] = false;
-        }
+    pub fn max_number_of_families(mut n: i32, mut reserved_seats: Vec<Vec<i32>>) -> i32 {
+        reserved_seats.sort_unstable_by_key(|v| (v[0], v[1]));
 
         let mut sum = 0;
-        for row in seats.iter_mut() {
+        for chunk in reserved_seats.chunk_by(|a, b| a[0] == b[0]) {
+            let mut row = [true; 10];
+            for c in chunk {
+                row[c[1] as usize - 1] = false;
+            }
             let avail = row[1] && row[2] && row[3] && row[4];
             sum += avail as usize;
             row[4] &= !avail;
@@ -15,12 +15,10 @@ impl Solution {
             sum += avail as usize;
             row[6] &= !avail;
             sum += (row[5] && row[6] && row[7] && row[8]) as usize;
-            println!("row {:?}", row);
-            println!("sum {}", sum);
+
+            n -= 1;
         }
-
-
-        sum as i32
+        sum as i32 + n * 2
     }
 }
 
