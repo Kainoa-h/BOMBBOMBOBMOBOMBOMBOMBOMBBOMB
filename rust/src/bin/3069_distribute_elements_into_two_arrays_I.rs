de17bc67800fb96f534cea0f200327d776245d9e
Kainoa-h <kainoahowj@gmail.com>
2026-08-20 15:57:49 +0800

    rust 3069: reuse nums for less heap allocation

--- rust/src/bin/3069_distribute_elements_into_two_arrays_I.rs
+++ rust/src/bin/3069_distribute_elements_into_two_arrays_I.rs
@@ -1,26 +1,30 @@
 impl Solution {
-    pub fn result_array(nums: Vec<i32>) -> Vec<i32> {
-        let mut v1 = Vec::with_capacity(nums.len());
-        let mut v2 = Vec::new();
+    pub fn result_array(mut nums: Vec<i32>) -> Vec<i32> {
+        let n = nums.len();
+        let mut v2 = Vec::with_capacity(n);
 
         let mut l1 = nums[0];
         let mut l2 = nums[1];
 
-        v1.push(l1);
         v2.push(l2);
+        let mut v1_len = 1;
 
-        for r in nums.into_iter().skip(2) {
+        for i in 2..n {
+            let x = nums[i];
             if l1 > l2 {
-                l1 = r;
-                v1.push(r);
-            } else {
-                l2 = r;
-                v2.push(r);
+                nums[v1_len] = x;
+                v1_len += 1;
+                l1 = x;
+            }
+            else {
+                v2.push(x);
+                l2 = x;
             }
         }
 
-        v1.append(&mut v2);
-        v1
+        nums.truncate(v1_len);
+        nums.append(&mut v2);
+        nums
     }
 }
 
