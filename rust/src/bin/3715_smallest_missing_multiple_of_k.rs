fd2bd1cefcc645d845e26c107aff63faf6d9ef57
Kainoa-h <kainoahowj@gmail.com>
2026-08-25 10:54:21 +0800

    rust 3715: brah

---
+++ rust/src/bin/3715_smallest_missing_multiple_of_k.rs
@@ -0,0 +1,10 @@
+impl Solution {
+    pub fn missing_multiple(nums: Vec<i32>, k: i32) -> i32 {
+    }
+}
+
+struct Solution {}
+
+fn main() {
+     Solution::missing_multiple(vec![8,2,3,4,6], 2);
+}
