727caf346b40eea30f0eee966481eed45b00728d
Kainoa-h <kainoahowj@gmail.com>
2026-08-26 13:57:11 +0800

    rust 2904: hais you can just compare strings...

--- rust/src/bin/2904_shortest_and_lexicographically_smallest_beautiful_string.rs
+++ rust/src/bin/2904_shortest_and_lexicographically_smallest_beautiful_string.rs
@@ -1,46 +1,40 @@
 impl Solution {
     pub fn shortest_beautiful_substring(s: String, k: i32) -> String {
         let bytes = s.as_bytes();
-        let mut start = 0;
-        let mut end = 0;
+        let (mut start, mut end) = (0, 0);
         let mut ones = 0;
-        let mut substring = &s[..];
-        let mut substring_binary_value = i64::MAX;
-        let mut curr_binary_value = 0;
+        let mut substring = None::<&str>;
         while end <= bytes.len() {
-            println!();
             if ones < k {
                 if end == bytes.len() {
                     break;
                 }
-                let x = (b'1' == bytes[end]) as i64;
-                curr_binary_value <<= 1;
-                curr_binary_value += x;
-                ones += x as i32;
+                ones += (b'1' == bytes[end]) as i32;
                 end += 1;
                 continue;
             }
 
-            if ones == k && (end - start) < substring.len()
-                || ((end - start) == substring.len() && curr_binary_value < substring_binary_value)
-            {
-                substring = &s[start..end];
-                substring_binary_value = curr_binary_value;
+            if ones == k {
+                let candidate = &s[start..end];
+                match substring {
+                    None => substring = Some(candidate),
+                    Some(cur)
+                        if cur.len() > candidate.len()
+                            || (cur.len() == candidate.len() && cur > candidate) =>
+                    {
+                        substring = Some(candidate)
+                    }
+                    _ => {}
+                }
             }
-            println!("substring: {}, value: {}", substring.to_owned(), substring_binary_value);
-            println!("start: {}, end: {}, len: {}", start, end, (end-start));
 
-            let x = (b'1' == bytes[start]) as i64;
-            println!("x: {}",x);
-            curr_binary_value -= x << (end - start - 1);
-            ones -= x as i32;
+            ones -= (b'1' == bytes[start]) as i32;
             start += 1;
         }
 
-        if substring_binary_value == i64::MAX {
-            "".to_owned()
-        } else {
-            substring.to_owned()
+        match substring {
+            None => "".to_owned(),
+            Some(x) => x.to_owned(),
         }
     }
 }
@@ -56,8 +50,12 @@ fn main() {
     //     Solution::shortest_beautiful_substring("001".to_owned(), 1),
     //     "1".to_owned()
     // );
+    // assert_eq!(
+    //     Solution::shortest_beautiful_substring("110101000010110101".to_owned(), 3),
+    //     "1011".to_owned()
+    // );
     assert_eq!(
-        Solution::shortest_beautiful_substring("110101000010110101".to_owned(), 3),
-        "1011".to_owned()
+        Solution::shortest_beautiful_substring("11011".to_owned(), 4),
+        "11011".to_owned()
     );
 }
