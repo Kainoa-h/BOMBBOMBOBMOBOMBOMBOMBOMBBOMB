1b0b97f89df6b5c73cfbdd9f2e7ce6f931043881
kainoa-h <kainoahowj@gmail.com>
2026-08-29 01:44:10 +0800

    rust 190: lol

--- rust/src/bin/190_reverse_bits.rs
+++ rust/src/bin/190_reverse_bits.rs
@@ -1,11 +1,6 @@
 impl Solution {
-    pub fn reverse_bits(mut n: i32) -> i32 {
-        let mut result = 0;
-        for _ in 0..32 {
-            result = (result << 1) | (n & 1);
-            n >>= 1;
-        }
-        result
+    pub fn reverse_bits(n: i32) -> i32 {
+        n.reverse_bits()
     }
 }
 
