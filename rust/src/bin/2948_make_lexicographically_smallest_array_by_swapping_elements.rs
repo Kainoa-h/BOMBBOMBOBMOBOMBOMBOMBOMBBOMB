fd918e8a6941ca03702332d2ccd1350b49a2830d
kainoa-h <kainoahowj@gmail.com>
2026-08-29 18:44:31 +0800

    rust 2948: no need for dsu, sorting & chunking gives us groups

--- rust/src/bin/2948_make_lexicographically_smallest_array_by_swapping_elements.rs
+++ rust/src/bin/2948_make_lexicographically_smallest_array_by_swapping_elements.rs
@@ -1,45 +1,18 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn lexicographically_smallest_array(nums: Vec<i32>, limit: i32) -> Vec<i32> {
-        let mut dsu = (0..nums.len()).collect::<Vec<usize>>();
-        let mut sorted = nums
-            .iter()
-            .enumerate()
-            .map(|x| (x.0, *x.1))
-            .collect::<Vec<(usize, i32)>>();
-        sorted.sort_unstable_by_key(|x| x.1);
-        fn find(dsu: &mut Vec<usize>, i: usize) -> usize {
-            if dsu[i] == i {
-                return i;
-            }
-            dsu[i] = find(dsu, dsu[i]);
-            dsu[i]
-        }
-
-        for w in sorted.windows(2) {
-            let (a, b) = (w[0], w[1]);
-            if b.1 - a.1 <= limit {
-                let b_root = find(&mut dsu, b.0);
-                let a_root = find(&mut dsu, a.0);
-                dsu[b_root] = a_root;
+        let mut indexes = (0..nums.len()).collect::<Vec<usize>>();
+        indexes.sort_unstable_by_key(|&x| nums[x]);
+        let mut result = vec![0; nums.len()];
+        for group in indexes.chunk_by(|&a, &b| nums[b] - nums[a] <= limit) {
+            let mut index_order_group = group.to_vec();
+            index_order_group.sort_unstable_by_key(|&x| x);
+            for i in 0..group.len() {
+                let val = nums[group[i]];
+                let idx = index_order_group[i];
+                result[idx] = val;
             }
         }
-
-        let mut map = HashMap::<usize,Vec<i32>>::new();
-        for (idx, &val) in nums.iter().enumerate() {
-            let root = find(&mut dsu, idx);
-            map.entry(root).or_default().push(val);
-        }
-
-        for l in map.values_mut() {
-            l.sort_unstable_by_key(|x|-x);
-        }
-
-        (0..nums.len()).map(|idx| {
-            let root = find(&mut dsu, idx);
-            map.get_mut(&root).unwrap().pop().unwrap()
-        }).collect()
+        result
     }
 }
 
