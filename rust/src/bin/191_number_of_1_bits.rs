f333022ada84e0150dfeb5375d75989bf85a17f5
kainoa-h <kainoahowj@gmail.com>
2026-08-30 23:06:35 +0800

    rust 191: built-in! compiles to popcnt instruction if supported

--- rust/src/bin/191_number_of_1_bits.rs
+++ rust/src/bin/191_number_of_1_bits.rs
@@ -1,11 +1,6 @@
 impl Solution {
-    pub fn hamming_weight(mut n: i32) -> i32 {
-        let mut hamming_weight = 0;
-        while n > 0 {
-            n &= n - 1;
-            hamming_weight += 1;
-        }
-        hamming_weight
+    pub fn hamming_weight(n: i32) -> i32 {
+        n.count_ones() as i32
     }
 }
 
