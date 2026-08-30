c538f6b7f6ae7bf0a1a2b2b606ab54d983ca6825
kainoa-h <kainoahowj@gmail.com>
2026-08-30 23:03:36 +0800

    rust 191: Brian Kernighan's Algo

--- rust/src/bin/191_number_of_1_bits.rs
+++ rust/src/bin/191_number_of_1_bits.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn hamming_weight(mut n: i32) -> i32 {
         let mut hamming_weight = 0;
-        for _ in 0..32 {
-            hamming_weight += n & 1;
-            n >>= 1;
+        while n > 0 {
+            n &= n - 1;
+            hamming_weight += 1;
         }
         hamming_weight
     }
 }
 
-struct Solution{}
+struct Solution {}
