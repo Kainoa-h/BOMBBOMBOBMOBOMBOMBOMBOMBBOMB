284eb8921ef5cfb843eb5c94800fe23f1259609c
Kainoa-h <kainoahowj@gmail.com>
2026-09-01 13:42:22 +0800

    rust 3568: minor simplifications

--- rust/src/bin/3568_minimum_moves_to_clean_the_classroom.rs
+++ rust/src/bin/3568_minimum_moves_to_clean_the_classroom.rs
@@ -11,29 +11,27 @@ struct State {
 impl Solution {
     pub fn min_moves(classroom: Vec<String>, energy: i32) -> i32 {
         let mut litter_map = HashMap::with_capacity(10);
-        let mut litter_mask = 0_u16;
         let mut count = 0;
-        let (mut start_row, mut start_col) = (0, 0);
+        let mut start = (0, 0);
         for (row_i, row) in classroom.iter().enumerate() {
             for (col_i, &byte) in row.as_bytes().iter().enumerate() {
                 if byte == b'L' {
                     litter_map.insert((row_i, col_i), count);
-                    litter_mask |= 1 << count;
                     count += 1;
                 } else if byte == b'S' {
-                    start_row = row_i;
-                    start_col = col_i;
+                    start = (row_i, col_i);
                 }
             }
         }
+        let litter_mask = (1 << count) - 1;
 
         let rows = classroom.len();
         let cols = classroom[0].len();
         let mut best_mask = vec![vec![HashMap::<u16, u16>::new(); cols]; rows];
         let mut queue = VecDeque::<State>::new();
         queue.push_back(State {
-            row: start_row,
-            col: start_col,
+            row: start.0,
+            col: start.1,
             steps: 0,
             energy: energy as u16,
             mask: 0,
@@ -41,7 +39,6 @@ impl Solution {
 
         while let Some(mut state) = queue.pop_front() {
             match classroom[state.row].as_bytes()[state.col] {
-                b'X' => continue,
                 b'R' => state.energy = energy as u16,
                 b'L' => {
                     state.mask |= 1 << *litter_map.get(&(state.row, state.col)).unwrap_or(&0) as u16
@@ -65,43 +62,27 @@ impl Solution {
             if state.energy == 0 {
                 continue;
             }
-            if state.row > 0 {
-                queue.push_back(State {
-                    row: state.row - 1,
-                    col: state.col,
-                    steps: state.steps + 1,
-                    energy: state.energy - 1,
-                    mask: state.mask,
-                });
+
+            for (dr, dc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                let nr = state.row as isize + dr;
+                let nc = state.col as isize + dc;
+
+                if nr >= 0
+                    && nr < rows as isize
+                    && nc >= 0
+                    && nc < cols as isize
+                    && classroom[nr as usize].as_bytes()[nc as usize] != b'X'
+                {
+                    queue.push_back(State {
+                        row: nr as usize,
+                        col: nc as usize,
+                        steps: state.steps + 1,
+                        energy: state.energy - 1,
+                        mask: state.mask,
+                    });
+                }
             }
-            if state.row < rows - 1 {
-                queue.push_back(State {
-                    row: state.row + 1,
-                    col: state.col,
-                    steps: state.steps + 1,
-                    energy: state.energy - 1,
-                    mask: state.mask,
-                });
-            }
-            if state.col > 0 {
-                queue.push_back(State {
-                    row: state.row,
-                    col: state.col - 1,
-                    steps: state.steps + 1,
-                    energy: state.energy - 1,
-                    mask: state.mask,
-                });
-            }
-            if state.col < cols - 1 {
-                queue.push_back(State {
-                    row: state.row,
-                    col: state.col + 1,
-                    steps: state.steps + 1,
-                    energy: state.energy - 1,
-                    mask: state.mask,
-                });
-            }
-        }
+       }
         -1
     }
 }
