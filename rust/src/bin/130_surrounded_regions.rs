f32da555d143945d6aacfd885f2e8dd18134f8f9
Kainoa-h <kainoahowj@gmail.com>
2026-09-02 22:13:48 +0800

    rust 130: less branching

--- rust/src/bin/130_surrounded_regions.rs
+++ rust/src/bin/130_surrounded_regions.rs
@@ -3,29 +3,35 @@ impl Solution {
         let rows = board.len();
         let cols = board[0].len();
 
-        fn mark_safe(board: &mut Vec<Vec<char>>, r: usize, c: usize) {
-            if board[r][c] == 'O' {
-                board[r][c] = 'S';
-                for (mr, mc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                    let (nr, nc) = (r as isize + mr, c as isize + mc);
-                    if (0..board.len() as isize).contains(&nr)
-                        && (0..board[0].len() as isize).contains(&nc)
-                        && board[nr as usize][nc as usize] == 'O'
-                    {
-                        mark_safe(board, nr as usize, nc as usize);
-                    }
+        fn mark_safe(board: &mut [Vec<char>], r: usize, c: usize) {
+            board[r][c] = 'S';
+            for (mr, mc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                let (nr, nc) = (r as isize + mr, c as isize + mc);
+                if (0..board.len() as isize).contains(&nr)
+                    && (0..board[0].len() as isize).contains(&nc)
+                    && board[nr as usize][nc as usize] == 'O'
+                {
+                    mark_safe(board, nr as usize, nc as usize);
                 }
             }
         }
 
         for i in 0..rows {
-            mark_safe(board, i, 0);
-            mark_safe(board, i, cols - 1);
+            if board[i][0] == 'O' {
+                mark_safe(board, i, 0);
+            }
+            if board[i][cols - 1] == 'O' {
+                mark_safe(board, i, cols - 1);
+            }
         }
 
-        for i in 0..cols {
-            mark_safe(board, 0, i);
-            mark_safe(board, rows - 1, i);
+        for i in 1..cols - 1 {
+            if board[0][i] == 'O' {
+                mark_safe(board, 0, i);
+            }
+            if board[rows - 1][i] == 'O' {
+                mark_safe(board, rows - 1, i);
+            }
         }
 
         for row in board {
