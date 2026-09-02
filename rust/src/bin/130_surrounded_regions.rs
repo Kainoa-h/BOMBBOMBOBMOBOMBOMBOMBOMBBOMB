d60ec144e5582a64f111c96ca9ca204ed0212a37
Kainoa-h <kainoahowj@gmail.com>
2026-09-02 22:04:42 +0800

    rust 130: mark safe first

--- rust/src/bin/130_surrounded_regions.rs
+++ rust/src/bin/130_surrounded_regions.rs
@@ -2,54 +2,38 @@ impl Solution {
     pub fn solve(board: &mut Vec<Vec<char>>) {
         let rows = board.len();
         let cols = board[0].len();
-        let mut visited_board = vec![vec![false; cols]; rows];
 
-        fn can_capture(
-            board: &mut Vec<Vec<char>>,
-            visited_board: &mut Vec<Vec<bool>>,
-            r: usize,
-            c: usize,
-            rows: usize,
-            cols: usize,
-        ) -> bool {
-            visited_board[r][c] = true;
-
-            let mut can = true;
-            for (rm, cm) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                let (nr, nc) = (r as isize + rm, c as isize + cm);
-                if (0..rows as isize).contains(&nr) && (0..cols as isize).contains(&nc) {
-                    if !visited_board[nr as usize][nc as usize]
-                        && board[nr as usize][nc as usize] == 'O' {
-                        can &= can_capture(board, visited_board, nr as usize, nc as usize, rows, cols)
+        fn mark_safe(board: &mut Vec<Vec<char>>, r: usize, c: usize) {
+            if board[r][c] == 'O' {
+                board[r][c] = 'S';
+                for (mr, mc) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+                    let (nr, nc) = (r as isize + mr, c as isize + mc);
+                    if (0..board.len() as isize).contains(&nr)
+                        && (0..board[0].len() as isize).contains(&nc)
+                        && board[nr as usize][nc as usize] == 'O'
+                    {
+                        mark_safe(board, nr as usize, nc as usize);
                     }
-                } else {
-                    can = false;
-                }
-            }
-            can
-        }
-
-        fn capture(board: &mut Vec<Vec<char>>, r: usize, c: usize, rows: usize, cols: usize) {
-            board[r][c] = 'X';
-            for (rm, cm) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                let (nr, nc) = (r as isize + rm, c as isize + cm);
-                if (0..rows as isize).contains(&nr)
-                    && (0..cols as isize).contains(&nc)
-                    && board[nr as usize][nc as usize] == 'O'
-                {
-                    capture(board, nr as usize, nc as usize, rows, cols);
                 }
             }
         }
 
-        for r in 1..rows - 1 {
-            for c in 1..cols - 1 {
-                let letter = board[r][c];
-                if letter == 'O'
-                    && !visited_board[r][c]
-                    && can_capture(board, &mut visited_board, r, c, rows, cols)
-                {
-                    capture(board, r, c, rows, cols);
+        for i in 0..rows {
+            mark_safe(board, i, 0);
+            mark_safe(board, i, cols - 1);
+        }
+
+        for i in 0..cols {
+            mark_safe(board, 0, i);
+            mark_safe(board, rows - 1, i);
+        }
+
+        for row in board {
+            for cell in row {
+                match *cell {
+                    'S' => *cell = 'O',
+                    'O' => *cell = 'X',
+                    _ => {}
                 }
             }
         }
