408ebb4f94bfef2e1c9b0a9215b807ae10ffe145
Kainoa-h <kainoahowj@gmail.com>
2026-09-02 22:34:11 +0800

    rust 2058: even fewer comparisons

--- rust/src/bin/2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points.rs
+++ rust/src/bin/2058_find_the_minimum_and_maximum_number_of_nodes_between_critical_points.rs
@@ -22,7 +22,7 @@ impl Solution {
 
         let mut first_critical = None::<i32>;
         let mut recent_critical = None::<i32>;
-        let (mut min_dist, mut max_dist) = (i32::MAX, 0);
+        let mut min_dist = i32::MAX;
         while let Some(node) = curr {
             let Some(next_node) = node.next.as_deref() else {
                 break;
@@ -31,13 +31,10 @@ impl Solution {
             if (node.val < next_node.val && node.val < prev_val)
                 || (node.val > next_node.val && node.val > prev_val)
             {
-                if let Some(critical) = first_critical {
-                    max_dist = index - critical;
-                } else {
-                    first_critical = Some(index);
-                }
                 if let Some(critical) = recent_critical {
                     min_dist = min_dist.min(index - critical);
+                } else {
+                    first_critical = Some(index);
                 }
                 recent_critical = Some(index);
             }
@@ -47,10 +44,10 @@ impl Solution {
             index += 1;
         }
 
-        if min_dist == i32::MAX {
-            vec![-1, -1]
-        } else {
-            vec![min_dist, max_dist]
+        match (first_critical, recent_critical) {
+            (Some(f), Some(l)) if f != l => vec![min_dist, l-f],
+            _=> vec![-1, -1]
+            
         }
     }
 }
