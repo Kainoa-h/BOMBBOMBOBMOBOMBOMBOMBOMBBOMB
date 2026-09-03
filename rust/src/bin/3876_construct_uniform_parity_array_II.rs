be6defeb96427de4424d0c7fdd71bfc038fa17d1
Kainoa-h <kainoahowj@gmail.com>
2026-09-03 11:04:21 +0800

    rust 3876

---
+++ rust/src/bin/3876_construct_uniform_parity_array_II.rs
@@ -0,0 +1,15 @@
+impl Solution {
+    pub fn uniform_array(nums1: Vec<i32>) -> bool {
+        let Some(smallest_odd) = nums1.iter().filter(|&&x|x % 2 == 1).min().cloned() else {
+            return true;
+        };
+
+        let Some(smallest) = nums1.iter().min().cloned() else {
+            return false;
+        };
+
+        smallest_odd == smallest || nums1.iter().filter(|&&x| x == smallest_odd).count() > 1
+    }
+}
+
+struct Solution {}
