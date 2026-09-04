7a73a969c923e796d9d53be872cac667c18c3bb5
kainoa-h <kainoahowj@gmail.com>
2026-09-05 00:25:13 +0800

    rust 201: simplier way to find common most sig bits

--- rust/src/bin/201_bitwise_and_of_numbers_range.rs
+++ rust/src/bin/201_bitwise_and_of_numbers_range.rs
@@ -1,21 +1,17 @@
 impl Solution {
-    pub fn range_bitwise_and(left: i32, right: i32) -> i32 {
+    pub fn range_bitwise_and(mut left: i32, mut right: i32) -> i32 {
         let lz = left.leading_zeros();
         let rz = right.leading_zeros();
         if lz != rz {
             return 0;
         }
-
-        let mut result = 0;
-        for i in (0..(32 - lz)).rev() {
-            let l = (left >> i) & 1;
-            let r = (right >> i) & 1;
-            if l != r {
-                break;
-            }
-            result |= l << i;
+        let mut shift = 0;
+        while right > left {
+            right >>= 1;
+            left >>= 1;
+            shift += 1;
         }
-        result
+        right << shift
     }
 }
 
