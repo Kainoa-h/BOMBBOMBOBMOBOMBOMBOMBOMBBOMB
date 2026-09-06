282b0ab2a52754cf42aabe9ea6645a08b99aa6fa
kainoa-h <kainoahowj@gmail.com>
2026-09-06 21:52:02 +0800

    rust 115: copied dp solution

--- rust/src/bin/115_distinct_subsequences.rs
+++ rust/src/bin/115_distinct_subsequences.rs
@@ -1,40 +1,17 @@
-use std::collections::HashMap;
-
 impl Solution {
     pub fn num_distinct(s: String, t: String) -> i32 {
-        let s = s.into_bytes();
-        let t = t.into_bytes();
-        let mut cache = HashMap::<(usize, usize), i32>::new();
-        fn combinations(
-            s_idx: usize,
-            t_idx: usize,
-            s: &[u8],
-            t: &[u8],
-            cache: &mut HashMap<(usize, usize), i32>,
-        ) -> i32 {
-            if t[t_idx..].len() == 1 {
-                let x = s[s_idx..].iter().filter(|&&c| c == t[t_idx]).count() as i32;
-                return x;
-            }
+        let mut dp = vec![0_i64; t.len()+1];
+        dp[0] = 1;
 
-            if s[s_idx..].len() == 1 {
-                return 0;
-            }
-
-            if let Some(&cached) = cache.get(&(s_idx,t_idx)) {
-                return cached
-            }
-            let mut calls = 0;
-            for (idx, &c) in s.iter().enumerate().skip(s_idx) {
-                if c == t[t_idx] {
-                    let x = combinations(idx + 1, t_idx + 1, s, t, cache);
-                    calls += x;
+        for ch in s.into_bytes() {
+            for idx in (0..t.len()).rev() {
+                if ch == t.as_bytes()[idx] {
+                    dp[idx + 1] += dp[idx];
                 }
             }
-            cache.insert((s_idx,t_idx), calls);
-            calls
         }
-        combinations(0, 0, &s, &t, &mut cache)
+
+        dp[t.len()] as i32
     }
 }
 
