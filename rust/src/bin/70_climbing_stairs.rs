fe6820b420d2f1c4b0a0915516293313d6205584
Kainoa-h <kainoahowj@gmail.com>
2026-09-07 13:40:33 +0800

    rust 70: simplier accumulation

--- rust/src/bin/70_climbing_stairs.rs
+++ rust/src/bin/70_climbing_stairs.rs
@@ -1,14 +1,10 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        let mut dp = vec![0; n as usize + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-
-        for i in 2..=n as usize {
-            dp[i] += dp[i - 1] + dp[i - 2];
+        let mut dp = (1,1);
+        for _ in 1..n {
+            dp = (dp.1, dp.0 + dp.1);
         }
-
-        dp[n as usize]
+        dp.1
     }
 }
 
