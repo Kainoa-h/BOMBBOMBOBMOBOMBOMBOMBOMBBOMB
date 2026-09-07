99672b0ce2a7c6795d3b2c4a6f54c0785f38cac1
Kainoa-h <kainoahowj@gmail.com>
2026-09-07 14:42:38 +0800

    rust 198: cleaner dp

--- rust/src/bin/198_house_robber.rs
+++ rust/src/bin/198_house_robber.rs
@@ -1,13 +1,12 @@
 impl Solution {
     pub fn rob(nums: Vec<i32>) -> i32 {
-        let mut dp = vec![0; nums.len()];
-        for idx in 0..dp.len() {
-            //underflow but wtv
-            let prev_prev_now = dp.get(idx - 2).unwrap_or(&0) + nums[idx];
-            let prev = *dp.get(idx-1).unwrap_or(&0);
-            dp[idx] = prev_prev_now.max(prev);
+        let mut dp = (0, 0);
+        for n in nums {
+            let a = dp.0 + n;
+            let b = dp.1;
+            dp = (dp.1, a.max(b));
         }
-        dp[nums.len() - 1]
+        dp.1
     }
 }
 
