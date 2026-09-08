ce329abccba69898a39ba38b0c2fc8c7ba723077
Kainoa-h <kainoahowj@gmail.com>
2026-09-08 15:11:30 +0800

    rust 322: bottom-up dp is so hard

--- rust/src/bin/322_coin_change.rs
+++ rust/src/bin/322_coin_change.rs
@@ -1,34 +1,18 @@
-use std::collections::HashSet;
-
 impl Solution {
-    pub fn coin_change(mut coins: Vec<i32>, mut amount: i32) -> i32 {
-        if amount == 0 {
-            return 0;
-        }
+    pub fn coin_change(coins: Vec<i32>, amount: i32) -> i32 {
+        let mut dp = vec![None::<i32>; amount as usize + 1];
+        dp[0] = Some(0);
 
-        coins.sort_unstable_by_key(|x| -x);
-        let mut seen_set = HashSet::<i32>::new();
-        let mut stack = vec![amount];
-        let mut depth = 0;
-        while !stack.is_empty() {
-            let mut new_stack = Vec::new();
-            for amt in stack {
-                if amt == 0 {
-                    return depth;
-                }
-
-                for &c in &coins {
-                    let diff = amt - c;
-                    if c <= amt && !seen_set.contains(&diff) {
-                        seen_set.insert(diff);
-                        new_stack.push(diff);
-                    }
+        for amt in 1..=amount as usize {
+            for &coin in &coins {
+                let coin = coin as usize;
+                if amt >= coin && let Some(from) = dp[amt - coin] {
+                    dp[amt] = Some(dp[amt].map_or(from + 1, |x| x.min(from + 1)));
                 }
             }
-            stack = new_stack;
-            depth += 1;
         }
-        -1
+
+        dp[amount as usize].unwrap_or(-1)
     }
 }
 
