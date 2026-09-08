4946ce98434191a8f45575894701abc053f627a3
kainoa-h <kainoahowj@gmail.com>
2026-09-08 11:05:25 +0800

    rust 3870

---
+++ rust/src/bin/3870_count_commas_in_range.rs
@@ -0,0 +1,11 @@
+impl Solution {
+    pub fn count_commas(n: i32) -> i32 {
+        if n < 1000 {
+            0
+        } else {
+            n - 1000 + 1
+        }
+    }
+}
+
+struct Solution {}
