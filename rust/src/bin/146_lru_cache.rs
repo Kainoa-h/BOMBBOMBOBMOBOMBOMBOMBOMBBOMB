e710c47c4eee04dce8749a0a9527091c7785e5e6
Kainoa-h <kainoahowj@gmail.com>
2026-09-10 13:11:43 +0800

    rust 146!: closer but still wrong. code is so messy...

--- rust/src/bin/146_lru_cache.rs
+++ rust/src/bin/146_lru_cache.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 struct Entry {
     key: i32,
     value: i32,
@@ -30,13 +30,10 @@ impl LRUCache {
         }
     }
 
-    fn get(&mut self, key: i32) -> i32 {
-        let Some(&entry_idx) = self.map.get(&key) else {
-            return -1;
-        };
+    fn unstich_throw_back(&mut self, entry_idx: usize) {
         let entry = self.entry_vec[entry_idx];
         if self.head == Some(entry_idx) {
-            self.head = entry.next;
+            self.head = entry.next.or(self.tail);
         }
         if let Some(entry_before_idx) = entry.prev {
             self.entry_vec[entry_before_idx].next = entry.next;
@@ -46,13 +43,41 @@ impl LRUCache {
         }
         self.entry_vec[self.tail.unwrap()].next = Some(entry_idx);
         self.entry_vec[entry_idx].next = None;
+        self.entry_vec[entry_idx].prev = self.tail;
         self.tail = Some(entry_idx);
+    }
 
+    fn get(&mut self, key: i32) -> i32 {
+        println!("---GET: {}---", key);
+        println!("{:?}", self.entry_vec);
+        println!("{:?}", self.map);
+        println!("head {:?}, tail {:?}", self.head, self.tail);
+        let Some(&entry_idx) = self.map.get(&key) else {
+            println!("---NOT FOUND---\n");
+            return -1;
+        };
+        if self.head == self.tail {
+            println!("---ONLY ONE---\n");
+            return self.entry_vec[self.head.unwrap()].value;
+        }
+        self.unstich_throw_back(entry_idx);
+
+        println!("{:?}", self.entry_vec);
+        println!("{:?}", self.map);
+        println!("head {:?}, tail {:?}", self.head, self.tail);
+        println!("---END---\n");
         self.entry_vec[entry_idx].value
     }
 
     fn put(&mut self, key: i32, value: i32) {
-        if self.len < self.size {
+        println!("---PUT: {}---", key);
+        println!("{:?}", self.entry_vec);
+        println!("{:?}", self.map);
+        println!("head {:?}, tail {:?}", self.head, self.tail);
+        
+        if let Some(&entry_idx) = self.map.get(&key) {
+            self.entry_vec[entry_idx].value = value;
+        } else if self.len < self.size {
             if let Some(tail) = self.tail {
                 self.entry_vec[tail].next = Some(self.len);
             } else {
@@ -67,12 +92,39 @@ impl LRUCache {
             self.map.insert(key, self.len);
             self.tail = Some(self.len);
             self.len += 1;
+            println!("{:?}", self.entry_vec);
+            println!("{:?}", self.map);
+            println!("head {:?}, tail {:?}", self.head, self.tail);
+            println!("---END_INSERT---\n");
             return;
         }
+
+        if self.head == self.tail && self.map.contains_key(&key) {
+            println!("{:?}", self.entry_vec);
+            println!("{:?}", self.map);
+            println!("head {:?}, tail {:?}", self.head, self.tail);
+            println!("---END_NO_CHANGE---\n");
+            return;
+        }
+
+        if let Some(&entry_idx) = self.map.get(&key) {
+            self.unstich_throw_back(entry_idx);
+            println!("{:?}", self.entry_vec);
+            println!("{:?}", self.map);
+            println!("head {:?}, tail {:?}", self.head, self.tail);
+            println!("---MOVE_BACK---\n");
+            return;
+        }
+
         let old_entry_idx = self.head.unwrap();
         let old_entry = self.entry_vec[old_entry_idx];
+        if let Some(entry_idx_after_old) = old_entry.next {
+            self.entry_vec[entry_idx_after_old].prev = None;
+        }
+
         self.map.remove(&old_entry.key);
-        self.head = old_entry.next;
+        self.map.insert(key, old_entry_idx);
+        self.head = old_entry.next.or(self.tail);
         self.entry_vec[old_entry_idx] = Entry {
             key,
             value,
@@ -83,5 +135,86 @@ impl LRUCache {
             self.entry_vec[tail].next = Some(old_entry_idx);
         }
         self.tail = Some(old_entry_idx);
+        println!("{:?}", self.entry_vec);
+        println!("{:?}", self.map);
+        println!("head {:?}, tail {:?}", self.head, self.tail);
+        println!("---END_REPLACE---\n");
+    }
+}
+
+fn main() {
+    // test1();
+    test2();
+    // test3();
+}
+
+fn test1() {
+    let instruct = [
+        "put", "put", "get", "put", "get", "put", "get", "get", "get",
+    ];
+    let params = [
+        (1, 1),
+        (2, 2),
+        (1, -1),
+        (3, 3),
+        (2, -1),
+        (4, 4),
+        (1, -1),
+        (3, -1),
+        (4, -1),
+    ];
+    let expected = [0, 0, 1, 0, -1, 0, -1, 3, 4];
+    let mut lru = LRUCache::new(2);
+
+    for ((&ins, param), exp) in instruct.iter().zip(params).zip(expected) {
+        if ins == "put" {
+            lru.put(param.0, param.1);
+            continue;
+        }
+        assert_eq!(exp, lru.get(param.0));
+    }
+}
+
+fn test2() {
+    let instruct = [ "put", "get", "put", "get", "get" ];
+    let params = [
+        (2, 2),
+        (2, -1),
+        (3, 3),
+        (2, -1),
+        (3, -1),
+    ];
+    let expected = [0, 2, 0, -1, 3];
+    let mut lru = LRUCache::new(1);
+
+    for ((&ins, param), exp) in instruct.iter().zip(params).zip(expected) {
+        if ins == "put" {
+            lru.put(param.0, param.1);
+            continue;
+        }
+        assert_eq!(exp, lru.get(param.0));
+    }
+}
+
+
+fn test3() {
+    let instruct = ["put","put","get","put","put","get"];
+    let params = [
+        (2, 1),
+        (2, 2),
+        (2, -1),
+        (1, 1),
+        (4, 4),
+        (2, -1),
+    ];
+    let expected = [0, 0, 2, 0, 0, -1];
+    let mut lru = LRUCache::new(2);
+
+    for ((&ins, param), exp) in instruct.iter().zip(params).zip(expected) {
+        if ins == "put" {
+            lru.put(param.0, param.1);
+            continue;
+        }
+        assert_eq!(exp, lru.get(param.0));
     }
 }
