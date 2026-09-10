8c2d31037d488c689fb7e63dde6944e4e0b44d7a
kainoa-h <kainoahowj@gmail.com>
2026-09-11 01:50:37 +0800

    rust 64: worse, but funny
    for the love of the game
    
    additional O(n) allocation over the iterative in-place version. But this
    looks crazy

--- rust/src/bin/64_minimum_path_sum.rs
+++ rust/src/bin/64_minimum_path_sum.rs
@@ -1,23 +1,22 @@
 impl Solution {
-    pub fn min_path_sum(mut grid: Vec<Vec<i32>>) -> i32 {
-        let rows = grid.len();
+    pub fn min_path_sum(grid: Vec<Vec<i32>>) -> i32 {
         let cols = grid[0].len();
-
-        for i in 1..rows {
-            grid[i][0] += grid[i - 1][0];
-        }
-
-        for i in 1..cols {
-            grid[0][i] += grid[0][i - 1];
-        }
-
-        for r_idx in 1..rows {
-            for c_idx in 1..cols {
-                grid[r_idx][c_idx] += grid[r_idx - 1][c_idx].min(grid[r_idx][c_idx - 1]);
-            }
-        }
-
-        grid[rows - 1][cols - 1]
+        grid.iter().skip(1).fold(
+            grid[0]
+                .iter()
+                .scan(0, |acc, &x| {
+                    *acc += x;
+                    Some(*acc)
+                })
+                .collect::<Vec<i32>>(),
+            |mut acc, row| {
+                acc[0] += row[0];
+                for i in 1..cols {
+                    acc[i] = acc[i].min(acc[i - 1]) + row[i];
+                }
+                acc
+            },
+        )[cols - 1]
     }
 }
 
