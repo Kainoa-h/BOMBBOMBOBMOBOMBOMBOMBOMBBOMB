1735fe8776f19a7efd25a549b72c71520d90f372
kainoa-h <kainoahowj@gmail.com>
2026-09-10 23:30:42 +0800

    rust 300: wthelly greedy + binary search approach

--- rust/src/bin/300_longest_increasing_subsequence.rs
+++ rust/src/bin/300_longest_increasing_subsequence.rs
@@ -1,16 +1,16 @@
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut dp = vec![1;nums.len()];
-        let mut max = 1;
-        for (idx, &n) in nums.iter().enumerate() {
-            for bi in (0..idx).rev() {
-                if nums[bi] < n {
-                    dp[idx] = dp[idx].max(1 + dp[bi]);
-                    max = max.max(dp[idx]);
-                }
+        let mut res = vec![nums[0]];
+        for n in nums.into_iter().skip(1) {
+            if res[res.len()-1] < n {
+                res.push(n);
+            } else {
+                let idx = res.partition_point(|&x| x < n);
+                res[idx] = n;
             }
+
         }
-        max
+        res.len() as i32
     }
 }
 
