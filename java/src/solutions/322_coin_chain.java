4b295785e5fe9951cccb651a9856afaf3459b30d
Kainoa-h <kainoahowj@gmail.com>
2026-09-15 13:37:53 +0800

    java 322: simplify with sentinel value

--- java/src/solutions/322_coin_chain.java
+++ java/src/solutions/322_coin_chain.java
@@ -2,24 +2,16 @@ import java.util.Arrays;
 
 class Solution {
   public int coinChange(int[] coins, int amount) {
-    if (amount == 0) {
-      return 0;
-    }
     int[] dp = new int[amount + 1];
-    dp[0] = 1;
+    Arrays.fill(dp, amount + 1);
+    dp[0] = 0;
     for (var i = 1; i < dp.length; i++) {
       for (var coin : coins) {
-        if (i < coin) {
+        if (i < coin)
           continue;
-        }
-        var start = i - coin;
-        if (dp[start] != 0)
-          if (dp[i] == 0)
-            dp[i] = dp[start] + 1;
-          else
-            dp[i] = Math.min(dp[i], dp[start] + 1);
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
       }
     }
-    return dp[amount] == 0 ? -1 : dp[amount] - 1;
+    return dp[amount] == amount + 1 ? -1 : dp[amount];
   }
 }
