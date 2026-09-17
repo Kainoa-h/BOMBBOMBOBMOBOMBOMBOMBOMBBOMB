c8422453ae5511b24301beb0b2b76239b6c1a473
Kainoa-h <kainoahowj@gmail.com>
2026-09-17 23:06:37 +0800

    rust 53: kadane's algo

--- rust/src/bin/53_maximum_subarray.rs
+++ rust/src/bin/53_maximum_subarray.rs
@@ -1,14 +1,12 @@
 impl Solution {
     pub fn max_sub_array(nums: Vec<i32>) -> i32 {
-        let mut sum = 0;
-        let mut min = 0;
-        let mut dp = i32::MIN;
-        for n in nums {
-            sum += n;
-            dp = dp.max(sum - min);
-            min = min.min(sum);
+        let mut cur_sum = nums[0];
+        let mut best = nums[0];
+        for n in nums.into_iter().skip(1) {
+            cur_sum = n.max(cur_sum + n);
+            best = best.max(cur_sum);
         }
-        dp
+        best
     }
 }
 
