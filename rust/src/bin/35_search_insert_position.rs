72a87d377b0341aab45dff0cad57f9b3e140ec5b
Kainoa-h <kainoahowj@gmail.com>
2026-09-17 22:27:50 +0800

    rust 35: fix bin search

--- rust/src/bin/35_search_insert_position.rs
+++ rust/src/bin/35_search_insert_position.rs
@@ -1,20 +1,20 @@
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
         let mut left = 0;
-        let mut right = nums.len() - 1;
+        let mut right = nums.len();
 
         while left < right {
-            let mid = (right + left) / 2;
+            let mid = left + (right - left) / 2;
 
             if nums[mid] == target {
                 return mid as i32;
             } else if nums[mid] < target {
                 left = mid.saturating_add(1);
             } else {
-                right = mid.saturating_sub(1);
+                right = mid;
             }
         }
-        left as i32 + i32::from(nums[left] < target)
+        left as i32
     }
 }
 
