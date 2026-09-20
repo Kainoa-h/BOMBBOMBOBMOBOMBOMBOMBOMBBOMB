c2e2c650f840bbcaae65d1fc455f0ed2f3f78b18
kainoa-h <kainoahowj@gmail.com>
2026-09-20 22:37:24 +0800

    rust 3498: simplier reversed degree

--- rust/src/bin/3498_reverse_degree_of_a_string.rs
+++ rust/src/bin/3498_reverse_degree_of_a_string.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn reverse_degree(s: String) -> i32 {
         s.as_bytes().iter().enumerate().fold(0, |acc, (idx, &b)| {
-            acc + ((idx as i32 + 1) * ((b - b'a') as i32 - 26).abs())
+            acc + ((idx as i32 + 1) * (b'z' - b + 1) as i32)
         })
     }
 }
