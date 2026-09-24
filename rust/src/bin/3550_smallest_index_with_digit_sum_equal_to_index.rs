7526336102cb2d56e57977bdfb51f3f7240ff42c
Kainoa-h <kainoahowj@gmail.com>
2026-09-24 17:35:13 +0800

    rust 3550: more iterators!

--- rust/src/bin/3550_smallest_index_with_digit_sum_equal_to_index.rs
+++ rust/src/bin/3550_smallest_index_with_digit_sum_equal_to_index.rs
@@ -1,17 +1,15 @@
 impl Solution {
     pub fn smallest_index(nums: Vec<i32>) -> i32 {
-        nums.into_iter().enumerate().find_map(|(idx, mut num)| {
+        let digit_sum = | mut num: i32| {
             let mut sum = 0;
             while num > 0 {
                 sum += num % 10;
                 num /= 10;
             }
-            if sum as usize == idx {
-                Some(idx as i32)
-            } else {
-                None
-            }
-        }).unwrap_or(-1)
+            sum
+        };
+
+        nums.into_iter().enumerate().position(|(i, n)| digit_sum(n) == i as i32).map_or(-1, |x| x as i32)
     }
 }
 
