bbb0abd1c13ed2fe5e8d1a0cf48c6f7c6d2b3ac7
kainoa-h <kainoahowj@gmail.com>
2026-09-26 17:03:50 +0800

    rust 1807: idiomatic improvements

--- rust/src/bin/1807_evaluate_the_bracket_pairs_of_a_string.rs
+++ rust/src/bin/1807_evaluate_the_bracket_pairs_of_a_string.rs
@@ -2,24 +2,28 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn evaluate(s: String, knowledge: Vec<Vec<String>>) -> String {
-        let dunno = [b'?'];
-        let mut k_map = HashMap::new();
-        for kv in &knowledge {
-            k_map.insert(kv[0].as_bytes(), kv[1].as_bytes());
-        }
+        let dunno: &[u8] = b"?";
+        let bytes = s.as_bytes();
+        let k_map =
+            knowledge
+                .iter()
+                .fold(HashMap::with_capacity(knowledge.len()), |mut map, kv| {
+                    map.insert(kv[0].as_bytes(), kv[1].as_bytes());
+                    map
+                });
 
-        let mut result = Vec::<u8>::new();
-        let mut start_idx = None::<usize>;
-        for (idx, &c) in s.as_bytes().iter().enumerate() {
-            if c == b'(' {
-                start_idx = Some(idx + 1);
-            } else if c == b')' {
-                let &value = k_map.get(&s.as_bytes()[start_idx.unwrap()..idx])
-                    .unwrap_or(&dunno.as_ref());
-                result.extend_from_slice(value);
-                start_idx = None;
-            } else if start_idx.is_none() {
-                result.push(c);
+        let mut result = Vec::<u8>::with_capacity(s.len());
+        let mut start_idx = s.len();
+        for (idx, &c) in bytes.iter().enumerate() {
+            match c {
+                b'(' => start_idx = idx + 1,
+                b')' => {
+                    let &value = k_map.get(&bytes[start_idx..idx]).unwrap_or(&dunno);
+                    result.extend_from_slice(value);
+                    start_idx = s.len();
+                }
+                _ if start_idx == s.len() => result.push(c),
+                _ => {}
             }
         }
 
